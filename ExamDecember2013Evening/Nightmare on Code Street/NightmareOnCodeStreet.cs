982635a8002bde3a96a9c19767b3fd25c9e4b6c4
puhabg <puhabg@live.com>
2014-10-25 13:52:37 +0300

    me
    Exam Excercise

---
+++ ExamDecember2013Evening/Nightmare on Code Street/NightmareOnCodeStreet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+class NightmareOnCodeStreet
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        int sum = 0;
+        int numberOfMembers = 0;
+        
+        while (true)
+        {
+            int temp = number % 10;
+            number /= 10;
+            sum += temp;
+            numberOfMembers++;
+        }
+
+        Console.WriteLine(numberOfMembers + " " + sum);
+
+    }
+}
+
---
+++ ExamDecember2013Evening/test248/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+class ThreeSixNine
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+        int C = int.Parse(Console.ReadLine());
+        BigInteger R = 0;
+        BigInteger answerR = 0;
+
+        if (B == 2)
+        {
+            R = A % C;
+        }
+
+        if (B == 4)
+        {
+           R = A + C;
+        }
+
+        if (B == 8)
+        {
+            R = (BigInteger)A * C;
+        }
+
+        if (R % 4 == 0)
+        {
+            answerR = R / 4;
+            Console.WriteLine(answerR);
+        }
+
+        if (R % 4 != 0)
+        {
+            answerR = R % 4;
+            Console.WriteLine(answerR);
+        }
+
+        Console.WriteLine(R);
+    }
+}
