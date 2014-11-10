a4a8d7820e602efc108d05fd903a390557ea1fc0
puhabg <puhabg@live.com>
2014-11-10 16:34:21 +0200

    Solved first part of maximal element in portion of array

---
+++ CSharpCourse2/03. Methods/09.MaximalElementInArrayPortion/MaximalElementInArrayPortion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*Write a method that return the maximal element in
+a portion of array of integers starting at given index.
+Using it write another method that sorts an array in
+ascending / descending order.*/
+
+class MaximalElementInArrayPortion
+{
+    static int MaximalElement(int[] array, int statrtIndex)
+    {
+        int maxElementPosition = statrtIndex;
+        int maxElementValue = array[statrtIndex];
+        for (int i = statrtIndex; i < array.Length - 1; i++)
+        {
+            if (maxElementValue > array[i + 1])
+            {
+                maxElementPosition = i;
+            }
+        }
+        return maxElementPosition;
+    }
+    static void Main()
+    {
+        int[] input = { 1, 22, 54, 2, 5, 76, 2, 178, 3, 4, 7, 8, 9, 23, 6, 3, 89, 3, 4, 67, 23, };
+        int startIndex = 9;
+        int positionMaxElement = MaximalElement(input, startIndex);
+        Console.WriteLine(" The element on position {0} has the maximal value strating from position {1}", 
+            positionMaxElement, startIndex);
+        Console.WriteLine("The maximal value is {0}", input[positionMaxElement]);
+    }
+}
+
