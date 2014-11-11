9ce81e23974306f459492b0a72717de06844c372
puhabg <puhabg@live.com>
2014-11-11 17:10:49 +0200

    Started sorting method for MaximalElementInArrayPortion

--- CSharpCourse2/03. Methods/09.MaximalElementInArrayPortion/MaximalElementInArrayPortion.cs
+++ CSharpCourse2/03. Methods/09.MaximalElementInArrayPortion/MaximalElementInArrayPortion.cs
@@ -19,6 +19,25 @@ class MaximalElementInArrayPortion
         }
         return maxElementPosition;
     }
+    static int[] SortAscending(int[] array)
+    {
+        int length = array.Length;
+        for (int i = 0; i < length; i++)
+        {
+            int temp = array[length - 1];
+            array[length - 1] = array[MaximalElement(array, length - 1)];
+            array[MaximalElement(array, 0)] = temp;
+            length--;
+        }
+        return array;
+    }
+    static void PrintArray(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
     static void Main()
     {
         int[] input = { 1, 22, 54, 2, 5, 76, 2, 178, 3, 4, 7, 8, 9, 23, 6, 3, 89, 3, 4, 67, 23, };
@@ -26,6 +45,8 @@ class MaximalElementInArrayPortion
         int positionMaxElement = MaximalElement(input, startIndex);
         Console.WriteLine(" The element on position {0} has the maximal value strating from position {1}", positionMaxElement, startIndex);
         Console.WriteLine("The maximal value is {0}", input[positionMaxElement]);
+        PrintArray(SortAscending(input));
+
     }
 }
 
