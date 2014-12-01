2e33cdf509b05e6aa49cbf27fa7ab9e571c7aa1f
puhabg <puhabg@live.com>
2014-12-01 16:32:51 +0200

    First task solved for possitive numbers

---
+++ CSharpCourse2/04.NumeralSystems/01.ConvertDecimalNumberToBinary/ConvertDecimalNumberToBinary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*Write a program to convert decimal numbers to their
+binary representation.*/
+
+class ConvertDecimalNumberToBinary
+{
+    static List<byte> NumberToBinary(int number)
+    {
+        List<byte> array = new List<byte>();
+        while (number > 0)
+        {
+            array.Add(Convert.ToByte(number & 1));
+            number >>= 1;
+        }
+        return array;
+    }
+    static void PrintBinaryRepresentation(List<byte> array)
+    {
+        for (int i = array.Count - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+        }
+    }
+    static void Main()
+    {
+        Console.Write("Enter decimal number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("The binary representation of the chosen number is: ");
+        PrintBinaryRepresentation(NumberToBinary(number));
+        Console.WriteLine();
+    }
+}
+
