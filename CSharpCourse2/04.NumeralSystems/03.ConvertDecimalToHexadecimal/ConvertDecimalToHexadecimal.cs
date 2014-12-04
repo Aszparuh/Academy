ec5cd9b02751ff87165e0879940d06e464e60b3f
puhabg <puhabg@live.com>
2014-12-04 15:04:22 +0200

    Started task 3

---
+++ CSharpCourse2/04.NumeralSystems/03.ConvertDecimalToHexadecimal/ConvertDecimalToHexadecimal.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class ConvertDecimalToHexadecimal
+{
+    static char[] ConvertToHexadecimal(int number)
+    {
+
+    }
+    static void Main()
+    {
+        Console.Write("Enter decimal number: ");
+        int number = int.Parse(Console.ReadLine());
+    }
+}
+
