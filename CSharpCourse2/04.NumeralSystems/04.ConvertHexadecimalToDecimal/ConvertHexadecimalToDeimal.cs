d6f16ce0ef4fdc548da65bfd581e3704730a4c09
puhabg <puhabg@live.com>
2014-12-05 20:29:47 +0200

    Method to delete characters 0 or x

--- CSharpCourse2/04.NumeralSystems/04.ConvertHexadecimalToDecimal/ConvertHexadecimalToDeimal.cs
+++ CSharpCourse2/04.NumeralSystems/04.ConvertHexadecimalToDecimal/ConvertHexadecimalToDeimal.cs
@@ -6,8 +6,20 @@ their decimal representation.*/
 
 class ConvertHexadecimalToDeimal
 {
+    static int ConvertToDecimal(string numberAsString)
+    {
+        if (numberAsString[0] == '0' && numberAsString[1] == 'x')
+        {
+            numberAsString = numberAsString.Substring(2);
+        }
+        Console.WriteLine(numberAsString);
+        return 0;
+    }
     static void Main()
     {
+        Console.Write("Enter number in hexadecimal: ");
+        string numberHex = Console.ReadLine();
+        ConvertToDecimal(numberHex);
 
     }
 }
