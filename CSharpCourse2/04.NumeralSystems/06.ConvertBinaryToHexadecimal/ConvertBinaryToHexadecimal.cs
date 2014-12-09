6b6cb9ba49d4fe46b921d669e50ceed6b6e0ce70
puhabg <puhabg@live.com>
2014-12-09 15:20:49 +0200

    Added method to write zeros

---
+++ CSharpCourse2/04.NumeralSystems/06.ConvertBinaryToHexadecimal/ConvertBinaryToHexadecimal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/*Write a program to convert binary numbers to
+hexadecimal numbers (directly).*/
+
+class ConvertBinaryToHexadecimal
+{
+    static string AddZeroes(string binaryNumber)
+    {
+        while (binaryNumber.Length % 4 != 0 || binaryNumber.Length < 4)
+        {
+            binaryNumber.Insert(0, "0");
+        }
+        return binaryNumber;
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter number in binary: ");
+        string binaryNumber = Console.ReadLine();
+        
+    }
+}
