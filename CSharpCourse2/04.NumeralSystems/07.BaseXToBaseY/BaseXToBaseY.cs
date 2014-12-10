6ddb01bb6e1d1c5b57dd3fa25b2e57b8c5875dee
puhabg <puhabg@live.com>
2014-12-10 13:42:04 +0200

    Started task convert from any numerl system to any other

---
+++ CSharpCourse2/04.NumeralSystems/07.BaseXToBaseY/BaseXToBaseY.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/*Write a program to convert from any numeral system
+of given base s to any other numeral system of base
+d (2 ≤ s, d ≤ 16).*/
+
+class BaseXToBaseY
+{
+    static void Main()
+    {
+
+    }
+}
+
