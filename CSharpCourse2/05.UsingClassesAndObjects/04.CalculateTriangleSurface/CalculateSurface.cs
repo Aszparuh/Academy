e5954761c5283a061fc806e5ad600e3a4a52825f
puhabg <puhabg@live.com>
2014-12-16 16:56:26 +0200

    Added menu

--- CSharpCourse2/05.UsingClassesAndObjects/04.CalculateTriangleSurface/CalculateSurface.cs
+++ CSharpCourse2/05.UsingClassesAndObjects/04.CalculateTriangleSurface/CalculateSurface.cs
@@ -22,13 +22,55 @@ class CalculateSurface
 
     static double CalculateTwoSidesAngle(double sideB, double sideC, double angleA)
     {
-        angleA = angleA * Math.PI / 180;
+        angleA = angleA * Math.PI / 180; // Convert to radians
         double area = (sideC * sideB) / 2 * Math.Sin(angleA);
         return area;
+    }
+    
+    static void PrintResult(double result)
+    {
+        Console.WriteLine("{0:0.00}",result);
     }
 
     static void Main()
     {
-
+        Console.WriteLine("Select option to calculate area of triangle: ");
+        Console.WriteLine("\n 1 for one side and the height \n 2 for three sides \n 3 for two sides and angle");
+        Console.Write("Enter: ");
+        int choice = int.Parse(Console.ReadLine());
+        Console.Clear();
+        
+        if (choice == 1)
+        {
+            Console.Write("Enter side: ");
+            double side = double.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            double height = double.Parse(Console.ReadLine());
+            PrintResult(CalculateSideHeight(side, height));
+        }
+        else if (choice == 2)
+        {
+            Console.Write("Enter side A: ");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.Write("Enter side B: ");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.Write("Enter side C: ");
+            double sideC = double.Parse(Console.ReadLine());
+            PrintResult(CalculateThreeSides(sideA, sideB, sideC));
+        }
+        else if (choice == 3)
+        {
+            Console.Write("Enter first side: ");
+            double sideA = double.Parse(Console.ReadLine());
+            Console.Write("Enter second side: ");
+            double sideB = double.Parse(Console.ReadLine());
+            Console.Write("Enter angle in degrees: ");
+            double angle = double.Parse(Console.ReadLine());
+            PrintResult(CalculateTwoSidesAngle(sideA, sideB, angle));
+        }
+        else
+        {
+            Console.WriteLine("There is no such option");
+        }
     }
 }
