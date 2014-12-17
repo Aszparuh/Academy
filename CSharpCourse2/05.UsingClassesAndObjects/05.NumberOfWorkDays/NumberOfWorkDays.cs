eca9f125593808d50aa8c3f7b84febec543d542a
puhabg <puhabg@live.com>
2014-12-17 13:18:08 +0200

    Added method to get the date

---
+++ CSharpCourse2/05.UsingClassesAndObjects/05.NumberOfWorkDays/NumberOfWorkDays.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*Write a method that calculates the number of
+workdays between today and given date, passed as
+parameter. Consider that workdays are all days from
+Monday to Friday except a fixed list of public
+holidays specified preliminary as array.*/
+
+class NumberOfWorkDays
+{
+    static DateTime GetEndDate()
+    {
+        DateTime endDate = new DateTime();
+        Console.Write("Enter year: ");
+        endDate = endDate.AddYears(int.Parse(Console.ReadLine()) - 1);
+        Console.Write("Enter month: ");
+        endDate = endDate.AddMonths(int.Parse(Console.ReadLine()) - 1);
+        Console.Write("Enter day of the month: ");
+        endDate = endDate.AddDays(int.Parse(Console.ReadLine()) - 1);
+        return endDate;
+    }
+
+    static void Main()
+    {
+        DateTime endDate = GetEndDate();
+        Console.WriteLine(endDate);
+    }
+}
