1b6d634429b24badee5dcd78e093f73106825b87
puhabg <puhabg@live.com>
2014-12-23 21:30:25 +0200

    Starts project for task 3

---
+++ CSharpCourse2/06.TextFiles/03.NumberingLines/GiveANumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/*Write a program that reads a text file and inserts line
+numbers in front of each of its lines. The result
+should be written to another text file.*/
+
+class GiveANumber
+{
+    static void Main()
+    {
+        string inputFilePath = @"../../TextFiles/inputFile.txt";
+        string outputFilePath = @"../../TextFiles/outputFile.txt";
+    }
+}
