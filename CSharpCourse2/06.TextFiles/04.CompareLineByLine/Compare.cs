7667544dbb0ecfc526a5d615dbb632eec4120c0f
puhabg <puhabg@live.com>
2014-12-24 13:20:27 +0200

    Solves task 4 compare text files line by line

--- CSharpCourse2/06.TextFiles/04.CompareLineByLine/Compare.cs
+++ CSharpCourse2/06.TextFiles/04.CompareLineByLine/Compare.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 /*Write a program that compares two text files line by
 line and prints the number of lines that are the same
@@ -7,8 +8,39 @@ the files have equal number of lines.*/
 
 class Compare
 {
+    static void CompareFiles(string firstFile, string secondFile)
+    {
+        int identicalLines = 0;
+        int diffLines = 0;
+        using (StreamReader firstFileReader = new StreamReader(firstFile))
+        {
+            using (StreamReader secondFileReader = new StreamReader(secondFile))
+            {
+                string firstFileLine = string.Empty;
+                while ((firstFileLine = firstFileReader.ReadLine()) != null)
+                {
+                    string secondFileLine = secondFileReader.ReadLine();
+                    if (firstFileLine == secondFileLine)
+                    {
+                        identicalLines++;
+                    }
+                    else
+                    {
+                        diffLines++;
+                    }
+                }
+            }
+        }
+        Console.WriteLine("Number of identical lines: {0}", identicalLines);
+        Console.WriteLine("Number of different lines: {0}", diffLines);
+        //There are 2 different lines in the files.
+        //number of lines 24.
+    }
+
     static void Main()
     {
-
+        string firstFilePath = @"../../TextFiles/textFile.txt";
+        string secondFilePath = @"../../TextFiles/anotherTextFile.txt";
+        CompareFiles(firstFilePath, secondFilePath);
     }
 }
