cf3f6482e0f575341c69cdca17f148f27538f294
puhabg <puhabg@live.com>
2014-12-26 14:39:05 +0200

    Starts project sorting list of strings

---
+++ CSharpCourse2/06.TextFiles/06.SortList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+/*Write a program that reads a text file containing a
+list of strings, sorts them and saves them to another
+text file. Example:
+Ivan  George
+Peter  Ivan
+Maria  Maria
+George  Peter*/
+
+class Program
+{
+    static void Main()
+    {
+    }
+}
