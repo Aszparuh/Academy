f50d6654ae8ec1afe073c1a2284fde5d68740cd0
puhabg <puhabg@live.com>
2014-12-26 15:45:39 +0200

    Adds method to create string array from text file

--- CSharpCourse2/06.TextFiles/06.SortList/SortList.cs
+++ CSharpCourse2/06.TextFiles/06.SortList/SortList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
 
 /*Write a program that reads a text file containing a
 list of strings, sorts them and saves them to another
@@ -11,7 +12,27 @@ George  Peter*/
 
 class SortList
 {
+    static List<string> GetStringArr(string inputPath)
+    {
+        List<string> listArr = new List<string>();
+        StreamReader reader = new StreamReader(inputPath);
+        using(reader)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                listArr.Add(line);
+            }
+        }
+
+        return listArr;
+    }
+
+
+
     static void Main()
     {
+        string inputPath = @"../../TextFiles/inputFile.txt";
+        string outputPath = @"../../TextFile/outputFile.txt";
     }
 }
