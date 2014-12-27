e1904e062284e1a84ac60d24b904e441642b9cfc
puhabg <puhabg@live.com>
2014-12-27 16:27:17 +0200

    Solves task 8 replace words

---
+++ CSharpCourse2/06.TextFiles/08.ReplaceWords/ReplaceWords.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+/*Modify the solution of the previous problem to
+replace only whole words (not substrings).*/
+
+class ReplaceWords
+{
+    static void Replace(string inputPath, string outputPath)
+    {
+        using (StreamReader reader = new StreamReader(inputPath))
+        {
+            using (StreamWriter writer = new StreamWriter(outputPath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    writer.WriteLine(Regex.Replace(line, @"\bstart\b", "finish"));
+                }
+            }
+        }
+    }
+
+    static void Main()
+    {
+        string inputPath = @"../../TextFiles/inputFile.txt";
+        string outputPath = @"../../TextFiles/outputFile.txt";
+        Replace(inputPath, outputPath);
+    }
+}
+
