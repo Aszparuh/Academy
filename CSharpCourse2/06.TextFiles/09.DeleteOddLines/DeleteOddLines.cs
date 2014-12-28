dbe09bf5e8ce4e30102fb9255cd671bd8b24ef7a
puhabg <puhabg@live.com>
2014-12-28 15:39:55 +0200

    Solves task 9 deletes odd lines

---
+++ CSharpCourse2/06.TextFiles/09.DeleteOddLines/DeleteOddLines.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+/*Write a program that deletes from given text file all
+odd lines. The result should be in the same file.*/
+
+class DeleteOddLines
+{
+    static List<string> GetEvenLines(string path)
+    {
+        List<string> evenLines = new List<string>();
+        int lineNumber = 1;
+        StreamReader reader = new StreamReader(path);
+        using (reader)
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (lineNumber % 2 == 0)
+                {
+                    evenLines.Add(line);
+                }
+
+                lineNumber++;
+            }
+        }
+
+        return evenLines;
+    }
+
+    static void WriteEvenLines(List<string> evenLines, string path)
+    {
+        StreamWriter writer = new StreamWriter(path);
+        using (writer)
+        {
+            for (int i = 0; i < evenLines.Count; i++)
+            {
+                writer.WriteLine(evenLines[i]);
+            }
+        }
+    }
+
+    static void Main()
+    {
+        string filePath = @"../../TextFiles/inputFile.txt";
+        WriteEvenLines(GetEvenLines(filePath), filePath);
+    }
+}
