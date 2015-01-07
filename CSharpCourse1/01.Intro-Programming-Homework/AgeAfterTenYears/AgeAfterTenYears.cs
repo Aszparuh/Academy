84b3e07cafe5563b727bca2dc49b765f7eef6e91
puhabg <puhabg@live.com>
2015-01-07 23:34:22 +0200

    Changes the solution

--- CSharpCourse1/01.Intro-Programming-Homework/AgeAfterTenYears/AgeAfterTenYears.cs
+++ CSharpCourse1/01.Intro-Programming-Homework/AgeAfterTenYears/AgeAfterTenYears.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 
-/*rite a program to read your age from the console and print how old you will be after 10 years.*/
+/*Write a program to read your birthday from the console and print how old you are now and how old you will be after 10 years.*/
 
 class AgeAfterTenYears
 {
     static void Main()
     {
-        Console.Write("Enter your age: ");
-        byte age = byte.Parse(Console.ReadLine()); //byte.Parse - Converts the string representation of a number to its Byte equivalent.
-        Console.WriteLine("After ten years you will be {0} years old.", age + 10);
+        DateTime birthDate = new DateTime();
+        Console.Write("Enter birth year: ");
+        birthDate = birthDate.AddYears(int.Parse(Console.ReadLine()) - 1);
+        Console.Write("Enter birth month: ");
+        birthDate = birthDate.AddMonths(int.Parse(Console.ReadLine()) - 1);
+        Console.Write("Enter birth day: ");
+        birthDate = birthDate.AddDays(int.Parse(Console.ReadLine()) - 1);
+
+        DateTime today = DateTime.Today;
+
+        //Calculate age today
+        int age;
+        age = today.Year - birthDate.Year;
+        if (age > 0)
+        {
+            age -= Convert.ToInt32(today.Date < birthDate.Date.AddYears(age));
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+        Console.WriteLine("You are {0} years old.",age);
+
+        //Calculate age in ten years
+        today = today.AddYears(10);
+
+        age = today.Year - birthDate.Year;
+        if (age > 0)
+        {
+            age -= Convert.ToInt32(today.Date < birthDate.Date.AddYears(age));
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+        Console.WriteLine("In ten years you will be {0} years old",age);
     }
 }
