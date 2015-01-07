a1643c4f974613070509cf281ad8ae3088277105
puhabg <puhabg@live.com>
2015-01-07 21:12:35 +0200

    Adds solution to Primitive Data Types

---
+++ CSharpCourse1/02.Primitive-Data-Types-And-Variables/DeclareVariables/DeclareVariables.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*Declare five variables choosing for each of them the most appropriate of the types 
+byte, sbyte, short, ushort, int, uint, long, ulong to represent the following values: 52130, -115, 4825932, 97, -10000.
+Choose a large enough type for each number to ensure it will fit in it. Try to compile the code.
+Submit the source code of your Visual Studio project as part of your homework submission.*/
+
+class DeclareVariables
+{
+    static void Main()
+    {
+        ushort firstNumber = 52130;
+        sbyte secondNumber = -115;
+        int thirdNumber = 4825932;
+        sbyte fourthNumber = 97;
+        short fifthNumber = -10000;
+
+        Console.WriteLine("First Number is: {0}", firstNumber);
+        Console.WriteLine("Second Number is: {0}", secondNumber);
+        Console.WriteLine("Third Number is: {0}", thirdNumber);
+        Console.WriteLine("Fourth Number is: {0}", fourthNumber);
+        Console.WriteLine("Fifth Number is: {0}", fifthNumber);
+    }
+}
