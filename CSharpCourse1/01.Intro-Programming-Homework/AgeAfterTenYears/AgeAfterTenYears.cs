003ff2c93475d5d00949632c9a9b9834d99b7f6a
puhabg <puhabg@live.com>
2015-01-07 16:30:42 +0200

    Adds Homework for Itroduction to Programming

---
+++ CSharpCourse1/01.Intro-Programming-Homework/AgeAfterTenYears/AgeAfterTenYears.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/*rite a program to read your age from the console and print how old you will be after 10 years.*/
+
+class AgeAfterTenYears
+{
+    static void Main()
+    {
+        Console.Write("Enter your age: ");
+        byte age = byte.Parse(Console.ReadLine()); //byte.Parse - Converts the string representation of a number to its Byte equivalent.
+        Console.WriteLine("After ten years you will be {0} years old.", age + 10);
+    }
+}
