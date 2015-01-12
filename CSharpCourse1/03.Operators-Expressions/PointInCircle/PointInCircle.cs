cd1f29dd11914bbcde7eaa03ee02f6b441ed7881
puhabg <puhabg@live.com>
2015-01-12 02:03:24 +0200

    Solves Point in a Circle

---
+++ CSharpCourse1/03.Operators-Expressions/PointInCircle/PointInCircle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*Write an expression that checks if given point (x, y) is inside a circle K({0, 0}, 2).*/
+
+class PointInCircle
+{
+    static void Main()
+    {
+        Console.Write("Enter x: ");
+        int x = int.Parse(Console.ReadLine());
+        Console.Write("Enter y: ");
+        int y = int.Parse(Console.ReadLine());
+        int radius = 2;
+
+        if ((x * x) + (y * y) <= radius * radius)
+        {
+            Console.WriteLine("The point is in the circle K({0, 0}, 2)");
+        }
+        else
+        {
+            Console.WriteLine("The point is not in the circle K({0, 0}, 2)");
+        }
+    }
+}
