cc4b767300fa17e650370aa2ed96529acecdf0d9
puhabg <puhabg@live.com>
2015-01-12 22:29:57 +0200

    Solves PrimeNumber, ExtractBit3, ExtractBitFromInteger

---
+++ CSharpCourse1/03.Operators-Expressions/PrimeNumberCheck/PrimeNumberCheck.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+/*Write an expression that checks if given positive integer number n 
+ * (n = 100) is prime (i.e. it is divisible without remainder only to itself and 1).*/
+
+class PrimeNumberCheck
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        bool isPrime = false;
+
+        if (number > 100)
+        {
+            Console.WriteLine("Your number is out of range.");
+        }
+        else if (number == 1)
+        {
+            Console.WriteLine(isPrime);
+        }
+        else if ((number == 2) || (number == 3) || (number == 5) || (number == 7))
+        {
+            isPrime = true;
+            Console.WriteLine(isPrime);
+        }
+        else if ((number % 2 == 0) || (number % 3 == 0) || (number % 5 == 0) || (number % 7 == 0) || (number % 10 == 0))
+        {
+            Console.WriteLine(isPrime);
+        }
+        else
+        {
+            isPrime = true;
+            Console.WriteLine(isPrime);
+        }
+    }
+}
