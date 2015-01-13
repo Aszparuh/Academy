3703d0d87b69122845a1442c406f1a3bd7bafdbd
puhabg <puhabg@live.com>
2015-01-13 22:11:57 +0200

    Solves CheckBitAtPosition and ModifyBitAtPosition

---
+++ CSharpCourse1/03.Operators-Expressions/CheckBitAtGivenPosition/CheckBit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/* Write a Boolean expression that returns if the bit at position p (counting from 0, starting from the right) in
+ * given integer number n, has value of 1.*/
+
+class CheckBit
+{
+    static void Main()
+    {
+        Console.Write("Enter value for n: ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter the position of the bit: ");
+        int b = int.Parse(Console.ReadLine());
+        int mask = 1 << b;
+        int maskAndI = mask & n;
+        int bit = maskAndI >> b;
+        bool isOne = false;
+
+        if (bit == 1)
+        {
+            isOne = true;
+        }
+
+        Console.WriteLine(isOne);
+    }
+}
---
+++ CSharpCourse1/03.Operators-Expressions/ModifyBitAtPosition/ModifyBit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/*We are given an integer number n, a bit value v (v=0 or 1) and a position p.
+Write a sequence of operators (a few lines of C# code) that modifies n to hold 
+the value v at the position p from the binary representation of n while preserving 
+all other bits in n.*/
+
+class ModifyBit
+{
+    static void Main()
+    {
+        Console.Write("Enter number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("Enter position from 0 to 32: ");
+        int position = int.Parse(Console.ReadLine());
+        Console.Write("Enter value 1 or 0: ");
+        int value = int.Parse(Console.ReadLine());
+        if (value != 1 && value != 0)
+        {
+            Console.WriteLine("Value should be 0 or 1.");
+        }
+        else
+        {
+            if (position > 32)
+            {
+                Console.WriteLine("Position should be from 0 to 32");
+            }
+            else
+            {
+                if (value == 1)
+                {
+                    int mask = 1 << position;
+                    int result = mask | number;
+
+                    Console.WriteLine("The bit on position {0} was changed to 1", position);
+                    Console.WriteLine("The result is: {0}", result);
+                    Console.WriteLine();
+                    Console.WriteLine("    Your number in binary: " + Convert.ToString(number, 2).PadLeft(32, '0'));
+                    Console.WriteLine("                     Mask: " + Convert.ToString(mask, 2).PadLeft(32, '0'));
+                    Console.WriteLine("  Number or Mask binary &: " + Convert.ToString(result, 2).PadLeft(32, '0'));
+                }
+                else
+                {
+                    int mask = ~(1 << position);
+                    int result = mask & number;
+
+                    Console.WriteLine("The bit on position {0} was changed to 0", position);
+                    Console.WriteLine("The result is: {0}", result);
+                    Console.WriteLine();
+                    Console.WriteLine("    Your number in binary: " + Convert.ToString(number, 2).PadLeft(32, '0'));
+                    Console.WriteLine("                     Mask: " + Convert.ToString(mask, 2).PadLeft(32, '0'));
+                    Console.WriteLine("  Number or Mask binary &: " + Convert.ToString(result, 2).PadLeft(32, '0'));
+                }
+            }
+        }
+    }
+}
