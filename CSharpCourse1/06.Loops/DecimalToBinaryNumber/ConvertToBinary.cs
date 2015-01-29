d1cbe70d4afed574b7511ea9fc590979107afd02
puhabg <puhabg@live.com>
2015-01-30 01:58:10 +0200

    Adds all the rest of the tasks

---
+++ CSharpCourse1/06.Loops/DecimalToBinaryNumber/ConvertToBinary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+/*Using loops write a program that converts an integer number to its binary representation.
+The input is entered as long. The output should be a variable of type string.
+Do not use the built-in .NET functionality.*/
+
+class ConvertToBinary
+{
+    static byte[] PosNumberToBinary(int number)
+    {
+        byte[] array = new byte[32];
+        for (int i = 0; number > 0; i++)
+        {
+            array[i] = Convert.ToByte(number & 1);
+            number >>= 1;
+        }
+
+        return array;
+    }
+
+    static byte[] NegNumberToBinary(int number)
+    {
+        byte[] array = new byte[32];
+        int temp = int.MaxValue + number + 1;
+        for (int i = 0; temp > 0; i++)
+        {
+            array[i] = Convert.ToByte(temp & 1);
+            temp >>= 1;
+        }
+        
+        array[31] = 1;
+        return array;
+    }
+
+    static void PrintBinaryRepresentation(byte[] array)
+    {
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Enter decimal number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine("The binary representation of the chosen number is: ");
+        if (number >= 0)
+        {
+            PrintBinaryRepresentation(PosNumberToBinary(number));
+        }
+        else
+        {
+            PrintBinaryRepresentation(NegNumberToBinary(number));
+        }
+        Console.WriteLine();
+    }
+}
---
+++ CSharpCourse1/06.Loops/TrailingZeroesInN!/CheckZeroes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/*Write a program that calculates with how many zeroes the factorial of a given number n has at its end.
+Your program should work well for very big numbers, e.g. n=100000.*/
+
+class CheckZeroes
+{
+    static void Main()
+    {
+        Console.Write("Enter N!: ");
+        string input = Console.ReadLine();
+        int n = int.Parse(input);
+
+        int count = 0;
+        int five = 5;
+        int denominator = 0;
+        int powerFive = 1;
+
+        while (denominator <= n)
+        {
+            denominator = (int)Math.Pow(five, powerFive);
+            count += n / denominator;
+            powerFive++;
+        }
+        Console.WriteLine(count);
+    }
+}
