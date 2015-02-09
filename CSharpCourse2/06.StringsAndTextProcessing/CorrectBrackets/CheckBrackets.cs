8e9a462ddf13a11647b8bbec98e76f445d2287a7
puhabg <puhabg@live.com>
2015-02-09 17:25:07 +0200

    Starts project for String And Text Processing

---
+++ CSharpCourse2/06.StringsAndTextProcessing/CorrectBrackets/CheckBrackets.cs
@@ -0,0 +1,49 @@
+﻿namespace CorrectBrackets
+{
+    /*Write a program to check if in a given expression the brackets are put correctly.*/
+
+    using System;
+
+    class CheckBrackets
+    {
+        static bool Check(string expr)
+        {
+            bool emptyBrackets = false;
+            bool inBrackets = false;
+            for (int i = 0; i < expr.Length; i++)
+            {
+                if (expr[i] == '(')
+                {
+                    inBrackets = true;
+                }
+                if (expr[i] == ')')
+                {
+                    if (inBrackets)
+                    {
+                        inBrackets = false;  
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            if (!inBrackets)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter expression with brackets: ");
+            string expression = Console.ReadLine();
+            Console.WriteLine(Check(expression));
+        }
+    }
+}
---
+++ CSharpCourse2/06.StringsAndTextProcessing/ReverseString/Reverse.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseString
+{
+    /*Write a program that reads a string, reverses it and prints the result at the console.*/
+
+    using System;
+ 
+    class Reverse
+    {
+        static string ReverseString(string someInput)
+        {
+            char[] stringChars = someInput.ToCharArray();
+            Array.Reverse(stringChars);
+            return new string(stringChars);
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter text: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(ReverseString(input));     
+        }
+    }
+}
