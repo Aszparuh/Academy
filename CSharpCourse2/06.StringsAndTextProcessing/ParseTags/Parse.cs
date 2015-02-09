97309c98a00a3cf7e2c34ecbfc6e101edd65ac9e
puhabg <puhabg@live.com>
2015-02-10 00:49:50 +0200

    Starts parse tags

---
+++ CSharpCourse2/06.StringsAndTextProcessing/ParseTags/Parse.cs
@@ -0,0 +1,17 @@
+﻿namespace ParseTags
+{
+    using System;
+
+    /*You are given a text. Write a program that changes the text in all regions surrounded by the 
+     * tags <upcase> and </upcase> to upper-case. The tags cannot be nested.*/
+
+    class Parse
+    {
+        static void Main()
+        {
+            Console.Write("Enter the text: ");
+            string text = Console.ReadLine();
+
+        }
+    }
+}
