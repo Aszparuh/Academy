03431af3674cae23036f455590e80ec76b7346fd
puhabg <puhabg@live.com>
2015-02-10 21:38:55 +0200

    Solves Compare Arrays, Compare Char Arrays, Maximal Sequence

---
+++ CSharpCourse2/01.Arrays/CompareArrays/Compare.cs
@@ -0,0 +1,50 @@
+﻿namespace CompareArrays
+{
+    using System;
+
+    /*Write a program that reads two integer arrays from the console and compares them element by element.*/
+
+    class Compare
+    {
+        static void Main()
+        {
+            Console.Write("Enter the length of the arrays: ");
+            int length = int.Parse(Console.ReadLine());
+            int[] firstArray = new int[length];
+            int[] secondArray = new int[length];
+            bool equals = true;
+
+            Console.WriteLine("Enter the numbers for the first array");
+
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter element {0}: ", i + 1);
+                firstArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Enter the numbers for the second array");
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Enter element {0}: ", i + 1);
+                secondArray[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    equals = false;
+                }
+            }
+
+            if (equals)
+            {
+                Console.WriteLine("The arrays are identical");
+            }
+            else
+            {
+                Console.WriteLine("The arrays are not identical");
+            }
+        }
+    }
+}
