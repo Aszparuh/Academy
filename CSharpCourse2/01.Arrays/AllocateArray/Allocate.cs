fa645f77c275eeb05eef83bd67f4c0ac9f566e30
puhabg <puhabg@live.com>
2015-02-10 21:13:53 +0200

    Solves  Allocate Array

---
+++ CSharpCourse2/01.Arrays/AllocateArray/Allocate.cs
@@ -0,0 +1,21 @@
+﻿namespace AllocateArray
+{
+    using System;
+
+    /*Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+    Print the obtained array on the console.*/
+
+    class Allocate
+    {
+        static void Main()
+        {
+            int[] array = new int[20];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 5;
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
