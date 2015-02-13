b42c93798de8d672e8198a87a22fe4f24c22a4b7
puhabg <puhabg@live.com>
2015-02-13 17:00:36 +0200

    Starts project for Extract e-mails

---
+++ CSharpCourse2/06.StringsAndTextProcessing/ExtractEmails/Extract.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtractEmails
+{
+    using System;
+
+    /*Write a program for extracting all email addresses from given text.
+     All sub-strings that match the format <identifier>@<host>…<domain> should be recognized as emails.*/
+
+    class Extract
+    {
+        static void Main()
+        {
+            Console.Write("Enter text: ");
+            string text = Console.ReadLine();
+        }
+    }
+}
