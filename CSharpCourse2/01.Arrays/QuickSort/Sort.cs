29217e988fdccb6c00c4427d1771eb506d681575
puhabg <puhabg@live.com>
2015-02-14 16:42:34 +0200

    Solves Merge Sort

---
+++ CSharpCourse2/01.Arrays/QuickSort/Sort.cs
@@ -0,0 +1,14 @@
+﻿namespace QuickSort
+{
+    using System;
+
+    /*Write a program that sorts an array of integers using the Quick sort algorithm.*/
+
+    class Sort
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
