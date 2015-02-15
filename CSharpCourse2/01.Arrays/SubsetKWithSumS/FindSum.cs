6152fad7e7968dedf7e14ff28cb4b72e39a67762
puhabg <puhabg@live.com>
2015-02-15 15:28:31 +0200

    Solves Subset with Sum S with K elements

---
+++ CSharpCourse2/01.Arrays/SubsetKWithSumS/FindSum.cs
@@ -0,0 +1,16 @@
+﻿namespace SubsetKWithSumS
+{
+    using System;
+
+    /*Write a program that reads three integer numbers N, K and S and an array of N 
+      elements from the console.
+      Find in the array a subset of K elements that have sum S or indicate about its absence.*/
+
+    class FindSum
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
--- CSharpCourse2/01.Arrays/SubsetWithSumS/FindSum.cs
+++ CSharpCourse2/01.Arrays/SubsetWithSumS/FindSum.cs
@@ -14,7 +14,9 @@
             //int N = int.Parse(Console.ReadLine());
             long[] input = new long[] { 2, 1, 2, 4, 3, 5, 2, 6 };
             long curSum = 0;
+            int numberOfElementsK = 3;
             bool notFound = false;
+            int counter = 0;
 
             //for (int i = 0; i < N; i++)
             //{
@@ -23,6 +25,7 @@
 
             for (int i = 1; i <= (int)Math.Pow(2, (double)input.Length) - 1; i++)
             {
+                counter = 0;
                 curSum = 0;
                 string template = Convert.ToString(i, 2);
                 string template1 = template.PadLeft(input.Length, '0');
@@ -32,10 +35,11 @@
                     if (template1[j].ToString() == "1")
                     {
                         curSum += input[j];
+                        counter++;
                     }
                 }
 
-                if (curSum == S)
+                if (curSum == S && counter == numberOfElementsK)
                 {
                     Console.WriteLine("Yes");
                     break;
