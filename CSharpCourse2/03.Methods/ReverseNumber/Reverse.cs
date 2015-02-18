4fd06f06591f8e8dc004c3a47108f17d126107f4
puhabg <puhabg@live.com>
2015-02-18 23:19:25 +0200

    Solves ReverseNumber

---
+++ CSharpCourse2/03.Methods/ReverseNumber/Reverse.cs
@@ -0,0 +1,39 @@
+﻿namespace ReverseNumber
+{
+    using System;
+    using System.Text;
+
+    /*Write a method that reverses the digits of given decimal number.
+             Example:
+             
+             input	  output
+             256	  652
+             123.45	  54.321*/
+
+    class Reverse
+    {
+        static string TakeInput()
+        {
+            Console.Write("Enter your number: ");
+            return Console.ReadLine();
+        }
+
+        static string ReverseNumber()
+        {
+            string numberAsString = TakeInput();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = numberAsString.Length - 1; i >= 0; i--)
+            {
+                sb.Append(numberAsString[i]);
+            }
+
+            return sb.ToString();
+        }
+
+        static void Main()
+        {
+            Console.WriteLine("Your number reversed is {0}", ReverseNumber());
+        }
+    }
+}
