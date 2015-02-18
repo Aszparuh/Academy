681447b8c0aa8ec612fb9248d718a294cd65e0d9
puhabg <puhabg@live.com>
2015-02-18 22:17:38 +0200

    Solves Say Hello, Get Largest Number

---
+++ CSharpCourse2/03.Methods/GetLargestNumber/LargestNum.cs
@@ -0,0 +1,37 @@
+﻿namespace GetLargestNumber
+{
+    using System;
+
+    /*Write a method GetMax() with two parameters that returns the larger of two integers.
+      Write a program that reads 3 integers from the console and prints the largest of them using the method GetMax().*/
+
+    class LargestNum
+    {
+        static int GetMax(int firstNumber, int secondNumber)
+        {
+            if (firstNumber > secondNumber)
+            {
+                return firstNumber;
+            }
+            else if (firstNumber < secondNumber)
+            {
+                return secondNumber;
+            }
+            else
+            {
+                return firstNumber;
+            }
+        }
+
+        static void Main()
+        {
+            Console.Write("Enter first number: ");
+            int firstNum = int.Parse(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            int secondNum = int.Parse(Console.ReadLine());
+            Console.Write("Enter third number: ");
+            int thirdNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("The biggest number is {0}.", GetMax(GetMax(firstNum, secondNum), thirdNum));
+        }
+    }
+}
---
+++ CSharpCourse2/03.Methods/SayHello/PrintHelloName.cs
@@ -0,0 +1,26 @@
+﻿namespace SayHello
+{
+    using System;
+
+    /*Write a method that asks the user for his name and prints “Hello, <name>”
+      Write a program to test this method.
+       Example:
+       
+       input	output
+       Peter	Hello, Peter!*/
+
+    class PrintHelloName
+    {
+        static void AskAndPrintName()
+        {
+            Console.Write("Type your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, {0}!", name);
+        }
+
+        static void Main()
+        {
+            AskAndPrintName();
+        }
+    }
+}
