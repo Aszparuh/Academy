a9516adeefe48aef9112315c5588cfa908dec8a4
puhabg <puhabg@live.com>
2015-02-23 21:02:13 +0200

    Starts project for Using Classes and Objects, Solves Leap year

---
+++ CSharpCourse2/05.UsingClassesAndObjects/LeapYear/CheckForLeapYear.cs
@@ -0,0 +1,25 @@
+﻿namespace LeapYear
+{
+    using System;
+
+    /*Write a program that reads a year from the console and checks whether it is a leap one.
+      Use System.DateTime.*/
+
+    class CheckForLeapYear
+    {
+        static void Main()
+        {
+            Console.Write("Enter year to check if it is a leap year: ");
+            int year = int.Parse(Console.ReadLine());
+            bool isLeap = DateTime.IsLeapYear(year);
+            if (isLeap)
+            {
+                Console.WriteLine("{0} is leap year.", year);
+            }
+            else
+            {
+                Console.WriteLine("{0} is not leap year.", year);
+            }
+        }
+    }
+}
