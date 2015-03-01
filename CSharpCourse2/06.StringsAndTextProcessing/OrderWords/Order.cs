f21c506f3e351bfc1d1c47fd6cbfb235b65f2a58
puhabg <puhabg@live.com>
2015-03-01 12:56:57 +0200

    Solves Order Words

---
+++ CSharpCourse2/06.StringsAndTextProcessing/OrderWords/Order.cs
@@ -0,0 +1,26 @@
+﻿namespace OrderWords
+{
+    using System;
+
+    /*Write a program that reads a list of words, separated by spaces 
+     * and prints the list in an alphabetical order.*/
+
+    class Order
+    {
+        static void Main()
+        {
+            string text = "Write a program that reads a list of words, separated by spaces and prints the list in an alphabetical order";
+            Console.WriteLine("Input string: ");
+            Console.WriteLine(text);
+            string[] words = text.Split(' ');
+            Array.Sort(words);
+            Console.WriteLine();
+            Console.WriteLine("Sorted words:");
+            foreach (string word in words)
+            {
+                Console.WriteLine("{0} ", word);
+            }
+            Console.WriteLine();
+        }
+    }
+}
