423dfe026d244aba4fd76bb21b265b267b622641
puhabg <puhabg@live.com>
2015-03-03 15:31:31 +0200

    Solves Download File

---
+++ CSharpCourse2/07.ExceptionHandling/DownloadFile/Download.cs
@@ -0,0 +1,45 @@
+﻿namespace DownloadFile
+{
+    using System;
+    using System.IO;
+    using System.Net;
+
+    /*Write a program that downloads a file from Internet (e.g. Ninja image) and stores
+      it the current directory.
+      Find in Google how to download files in C#.
+      Be sure to catch all exceptions and to free any used resources in the finally block.*/
+
+    class Download
+    {
+        static void Main()
+        {
+            string sourcePath = @"http://telerikacademy.com/Content/Images/news-img01.png";
+            string localFilePath = Path.GetFileName(sourcePath);
+
+            WebClient webClient = new WebClient();
+
+            using (webClient)
+            {
+                try
+                {
+                    webClient.DownloadFile(sourcePath, localFilePath);
+                    Console.WriteLine("File has been downloaded.");
+                }
+                catch (WebException exception)
+                {
+                    Console.Write(exception.Message);
+                    if (exception.InnerException != null)
+                    {
+                        Console.WriteLine(" " + exception.InnerException.Message);
+                    }
+                    else
+                        Console.WriteLine();
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Erorr {0}", exception.InnerException.Message);
+                }
+            }
+        }
+    }
+}
