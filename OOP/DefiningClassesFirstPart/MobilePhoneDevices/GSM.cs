b8d0b77079e819c93a80191a8bd5c6a6e6091213
puhabg <puhabg@live.com>
2015-03-10 16:24:20 +0200

    Starts project for Mobile Phones OOP

---
+++ OOP/DefiningClassesFirstPart/MobilePhoneDevices/GSM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhoneDevices
+{
+    class GSM
+    {
+        private string manufacturer;
+        private string model;
+        private string owner;
+        private decimal? price;
+        private Battery battery;
+        private Display display;
+
+        public GSM(string manufacturer, string model)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.owner = null;
+            this.price = null;
+            this.battery = null;
+            this.display = null;
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public string Manufacturer
+        {
+            get { return this.manufacturer; }
+            set { this.manufacturer = value; }
+        }
+
+        public string Owner
+        {
+            get { return this.owner; }
+            set { this.owner = value; }
+        }
+    }
+}
