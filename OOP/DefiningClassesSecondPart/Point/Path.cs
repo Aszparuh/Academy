ade832a0766b5515a2136ed86c80dbfdf1e2b5d7
puhabg <puhabg@live.com>
2015-03-16 23:31:23 +0200

    Adds class Path - sotres sequence of Points3D

---
+++ OOP/DefiningClassesSecondPart/Point/Path.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Point
+{
+    public class Path
+    {
+        private List<Point3D> pathList;
+
+        public Path()
+        {
+            this.pathList = new List<Point3D>();
+        }
+
+        public void AddPoint(Point3D point)
+        {
+            this.pathList.Add(point);
+        }
+
+        public void ClearPath()
+        {
+            this.pathList.Clear();
+        }
+
+        public List<Point3D> GetPath()
+        {
+            return this.pathList;
+        }
+    }
+}
