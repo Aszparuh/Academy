5e196f2f9893dfa73f3376f339763676b193eaba
puhabg <puhabg@live.com>
2015-03-19 15:29:05 +0200

    Adds indexer

--- OOP/DefiningClassesSecondPart/Matrix/GenMatrix.cs
+++ OOP/DefiningClassesSecondPart/Matrix/GenMatrix.cs
@@ -38,13 +38,57 @@
             }
         }
 
+        public T[,] GetMatrix
+        {
+            get {return this.myMatrix; }
+        }
+
         //Constructors
         public GenMatrix(int rows, int cols)
         {
             this.Rows = rows;
             this.Cols = cols;
-
+            this.myMatrix = new T[Rows, Cols];
         }
 
+        public GenMatrix(T[,] readyMatrix)
+        {
+            this.myMatrix = readyMatrix;
+            this.rows = readyMatrix.GetLength(0);
+            this.cols = readyMatrix.GetLength(1);
+        }
+
+        //Indexer
+        public T this[int indexRows, int indexCols]
+        {
+            get
+            {
+                if (indexRows > this.rows - 1 || indexRows < 0)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+
+                if (indexCols > this.cols - 1 || indexCols < 0)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+
+                return this.myMatrix[indexRows, indexCols];
+            }
+            set
+            {
+                if (indexRows > this.rows - 1 || indexRows < 0)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+
+                if (indexCols > this.cols - 1 || indexCols < 0)
+                {
+                    throw new IndexOutOfRangeException("Invalid index.");
+                }
+
+                this.myMatrix[indexRows, indexCols] = value;
+            }
+        }
     }
 }
