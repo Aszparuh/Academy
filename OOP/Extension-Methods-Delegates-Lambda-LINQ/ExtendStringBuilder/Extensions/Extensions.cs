3045c49f47d8abbad83ef0b0aeafca5eb16e0bc9
puhabg <puhabg@live.com>
2015-03-22 13:52:59 +0200

    Adds Extending StringBuilder with Substring

---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ/ExtendStringBuilder/Extensions/Extensions.cs
@@ -0,0 +1,20 @@
+﻿namespace ExtendStringBuilder.Extensions
+{
+    using System;
+    using System.Text;
+
+    public static class Extensions
+    {
+        public static StringBuilder Substring(this StringBuilder sb, int startIndex, int length)
+        {
+            if (startIndex >= sb.Length || startIndex + length > sb.Length)
+            {
+                throw new IndexOutOfRangeException("Invalid index or length");
+            }
+
+            var sbAsString = sb.ToString();
+            string result = sbAsString.Substring(startIndex, length);
+            return new StringBuilder(result);
+        }
+    }
+}
---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ/ExtendStringBuilder/TestStringBuilderSubstring.cs
@@ -0,0 +1,15 @@
+﻿namespace ExtendStringBuilder
+{
+    using System;
+    using System.Text;
+    using ExtendStringBuilder.Extensions;
+
+    class TestStringBuilderSubstring
+    {
+        static void Main()
+        {
+            var sb = new StringBuilder("Some text");
+            Console.WriteLine(sb.Substring(5, 4));
+        }
+    }
+}
