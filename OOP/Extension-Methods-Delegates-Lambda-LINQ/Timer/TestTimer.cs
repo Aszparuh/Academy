17ed47ddc1eb88f9baf041144e9c35b1a92dce65
puhabg <puhabg@live.com>
2015-03-24 15:52:26 +0200

    Starts project for Timer

---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ/Timer/TestTimer.cs
@@ -0,0 +1,19 @@
+﻿namespace Timer
+{
+    using System;
+
+    class TestTimer
+    {
+        static void Main()
+        {
+            var tm = new Timer(8);
+            tm.EventStart(); 
+           
+        }
+
+        public static void EventTriger()
+        {
+            Console.WriteLine("Something happened");
+        }
+    }
+}
---
+++ OOP/Extension-Methods-Delegates-Lambda-LINQ/Timer/Timer.cs
@@ -0,0 +1,41 @@
+﻿namespace Timer
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public delegate void TimerEvent();
+
+    class Timer
+    {
+        private readonly TimerEvent someEvent;
+        private int intervalSec;
+
+        public Timer(int interval)
+        {
+            this.IntervalSec = interval;
+        }
+
+        public int IntervalSec
+        {
+            get { return this.intervalSec; }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Interval must be possitive number");
+                }
+                this.intervalSec = value;
+            }
+        }
+
+        public void EventStart()
+        {
+            while (true)
+            {
+                someEvent();
+                Thread.Sleep(this.intervalSec * 1000); //1 milisecond = 0.001 seconds
+            }
+        }
+    }
+}
