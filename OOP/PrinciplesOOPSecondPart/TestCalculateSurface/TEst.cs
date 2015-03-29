c208a13219d2ebb73984689bc1b7a4108fbc2d08
puhabg <puhabg@live.com>
2015-03-29 17:46:12 +0300

    Solves Shapes

---
+++ OOP/PrinciplesOOPSecondPart/TestCalculateSurface/TEst.cs
@@ -0,0 +1,16 @@
+﻿namespace TestCalculateSurface
+{
+    using System;
+    using Shapes.Models;
+    class Test
+    {
+        static void Main()
+        {
+            Shape[] shapes = new Shape[] { new Square(2.5), new Rectangle(3, 4.7), new Triangle(8, 3.5) };
+            foreach (var shape in shapes)
+            {
+                Console.WriteLine("The surface of {0} is:  {1}", shape.GetType(), shape.CalculateSurface());
+            }
+        }
+    }
+}
