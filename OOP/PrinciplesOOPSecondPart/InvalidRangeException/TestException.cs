46e0a6b13ca1f8a26ac2da16c2503ccecb80007b
puhabg <puhabg@live.com>
2015-03-31 14:59:38 +0300

    Solves Custom Exception

---
+++ OOP/PrinciplesOOPSecondPart/InvalidRangeException/TestException.cs
@@ -0,0 +1,28 @@
+﻿namespace InvalidRangeException
+{
+    using System;
+    using System.Globalization;
+
+    class TestException
+    {
+        static void Main()
+        {
+            int chosenNumber = int.Parse(Console.ReadLine());
+
+            if (chosenNumber < 1 || chosenNumber > 100)
+            {
+                throw new InvalidRangeException<int>(string.Format("Number is not in the range [{0}..{1}]",1,100), 1, 100);
+            }
+
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+
+            var startDate = new DateTime(1980, 1, 1);
+            var endDate = new DateTime(2013, 12, 31);
+
+            if (date < startDate || date > endDate)
+            {
+                throw new InvalidRangeException<DateTime>(string.Format("Date must be in range {0} - {1}", startDate, endDate), startDate, endDate);
+            }
+        }
+    }
+}
