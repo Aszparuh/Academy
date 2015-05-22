51b827dd08647844573bcbbe2fd6d7e6c7b71e63
puhabg <puhabg@live.com>
2015-05-22 16:33:45 +0300

    Changes for jslint

--- Javascript Fundamentals/Data Types and Variables/scripts/alltasks.js
+++ Javascript Fundamentals/Data Types and Variables/scripts/alltasks.js
@@ -1,23 +1,23 @@
 //task1
 var integer = 5,
-	floating = 5.5,
+    floating = 5.5,
 //task2
-	string = '\'How you doin\'?\', Joey said\'',
-	array = ['first','second','third'],
-	boolean = true,
-	car = {brand:"Fiat", model:500, color:"white"},
-	valueNull = null,
-	valueUndefined = undefined;
+    string = '\'How you doin\'?\', Joey said\'',
+    array = ['first', 'second', 'third'],
+    boolean = true,
+    car = {brand: "Fiat", model: 500, color: "white"},
+    valueNull = null,
+    valueUndefined = undefined;
 //task3
-jsConsole.writeLine(typeof(integer) + ' ' + integer);
-jsConsole.writeLine(typeof(floating) + ' ' + floating);
-jsConsole.writeLine(typeof(string) + ' ' + string);
-jsConsole.writeLine(typeof(array) + ' ' + array);
-jsConsole.writeLine(typeof(boolean) + ' ' + boolean);
-jsConsole.writeLine(typeof(car) + ' ' + car);
+jsConsole.writeLine(typeof integer + ' ' + integer);
+jsConsole.writeLine(typeof floating + ' ' + floating);
+jsConsole.writeLine(typeof string + ' ' + string);
+jsConsole.writeLine(typeof array + ' ' + array);
+jsConsole.writeLine(typeof boolean + ' ' + boolean);
+jsConsole.writeLine(typeof car + ' ' + car);
 jsConsole.writeLine('object brand:' + ' ' + car.brand);
 jsConsole.writeLine('object model:' + ' ' + car.model);
 jsConsole.writeLine('object color:' + ' ' + car.color);
 //task4
-jsConsole.writeLine(typeof(valueNull) + ' ' + valueNull);
-jsConsole.writeLine(typeof(valueUndefined) + ' ' + valueUndefined);
+jsConsole.writeLine(typeof valueNull + ' ' + valueNull);
+jsConsole.writeLine(typeof valueUndefined + ' ' + valueUndefined);
