2ef17510fe0e3708e03ea1d47215d769ef19059f
puhabg <puhabg@live.com>
2015-05-24 13:50:47 +0300

    Adds titles

--- Javascript Fundamentals/Operators and Expressions/scripts/calculateArea.js
+++ Javascript Fundamentals/Operators and Expressions/scripts/calculateArea.js
@@ -1,8 +1,6 @@
 function CalculateArea() {
 	var width = jsConsole.readFloat("#width");
 	var height = jsConsole.readFloat("#height");
-	console.log(width);
-	console.log(height);
 
 	if ((width <= 0) || (height <= 0)) {
 		jsConsole.writeLine('Widht or height cannot be equal or less than zero.');
