e6d3e9ef74d4f44f3903740fccc4ba6e0a67aac3
puhabg <puhabg@live.com>
2015-05-26 13:55:28 +0300

    Solves Point in Circle and outside Rectangle

--- Javascript Fundamentals/Operators and Expressions/scripts/checkPointCircleRectangle.js
+++ Javascript Fundamentals/Operators and Expressions/scripts/checkPointCircleRectangle.js
@@ -1,3 +1,20 @@
 function CheckPoint() {
-	
+	var x = jsConsole.readFloat("#pointX");
+	var y = jsConsole.readFloat("#pointY");
+	var radius = 3;
+
+	//Check if the point is in the circle K( (1,1), 3).
+	var xToCenter = x - 1;
+	var yToCenter = y - 1;
+	var isInCircle = ((xToCenter * xToCenter) + (yToCenter * yToCenter)) <= radius * radius;
+
+	//Check if the point is outside the rectangle R(top=1, left=-1, width=6, height=2).
+	var isOut = (x > 5) || (x < -1) || (y > 1) || (y < -1);
+
+	if (isInCircle && isOut) {
+		jsConsole.writeLine('yes');
+	}
+	else {
+		jsConsole.writeLine('no');
+	}
 }
