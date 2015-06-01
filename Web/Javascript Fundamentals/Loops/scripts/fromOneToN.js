0332df3d9e0b6dddd1d52e15899bd9fbfecb17de
puhabg <puhabg@live.com>
2015-06-01 13:29:38 +0300

    Solves both for negative numbers

--- Web/Javascript Fundamentals/Loops/scripts/fromOneToN.js
+++ Web/Javascript Fundamentals/Loops/scripts/fromOneToN.js
@@ -1,7 +1,15 @@
 function CountToN() {
     var n = jsConsole.readInteger("#number");
     var i;
-    for (i = 1; i <= n; i+= 1) {
-        jsConsole.writeLine(i);
+
+    if (n >= 1) {
+        for (i = 1; i <= n; i += 1) {
+            jsConsole.writeLine(i);
+        }
+    }
+    else {
+        for (i = 1; i >= n; i -= 1) {
+            jsConsole.writeLine(i);
+        }
     }
 }
--- Web/Javascript Fundamentals/Loops/scripts/numbersNotDivisible.js
+++ Web/Javascript Fundamentals/Loops/scripts/numbersNotDivisible.js
@@ -1,9 +1,19 @@
 function FindNums() {
     var n = jsConsole.readInteger("#number");
     var i;
-    for (i = 1; i <= n; i += 1) {
-        if ((i % 3) && (i % 7)) {
-            jsConsole.writeLine(i);
+
+    if (i >= 1) {
+        for (i = 1; i <= n; i += 1) {
+            if ((i % 3) && (i % 7)) {
+                jsConsole.writeLine(i);
+            }
+        }
+    }
+    else {
+        for (i = 1; i >= n; i -= 1) {
+            if ((i % 3) && (i % 7)) {
+                jsConsole.writeLine(i);
+            }
         }
     }
 }
