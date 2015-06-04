02227f5e96d1e31575885e24478785b3123ca412
puhabg <puhabg@live.com>
2015-06-04 13:47:56 +0300

    Solves Reverse Number

---
+++ Web/Javascript Fundamentals/Functions/scripts/reverseNumber.js
@@ -0,0 +1,12 @@
+function Reverse(input) {
+    var result='';
+    for (var i = input.length - 1; i >= 0; i -= 1) {
+        result += input[i];
+    }
+
+    return result;
+}
+function Print() {
+    var input = jsConsole.read("#number");
+    jsConsole.writeLine(Reverse(input));
+}
