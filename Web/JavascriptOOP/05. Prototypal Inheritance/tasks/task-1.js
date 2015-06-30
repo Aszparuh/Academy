d007dfdcbfd09f05cb3e39813c9b45531c687752
puhabg <puhabg@live.com>
2015-06-30 17:10:12 +0300

    Solves task for DOM element

--- Web/JavascriptOOP/05. Prototypal Inheritance/tasks/task-1.js
+++ Web/JavascriptOOP/05. Prototypal Inheritance/tasks/task-1.js
@@ -63,64 +63,137 @@ Outputs:
 function solve() {
 	var domElement = (function () {
 
-		function checkTypeName(value, itemToCheck) {
-			if (typeof value !== 'string') {
-				throw 'Type should be string';
-			}
-			if (value === '') {
-				throw 'Type should not be empty string';
-			}
-			if (itemToCheck === 'type') {
-				if (!(/^[a-zA-Z]+$/.test(value))) {
-					throw 'Type contains bad characters';
-				}
-			}
-			else {
-				if (/\d/.test(value)) {
-					throw 'Name contains digits'
-				}
+	function checkTypeName(value, itemToCheck) {
+		if (typeof value !== 'string') {
+			throw 'Type should be string';
+		}
+		if (value === '') {
+			throw 'Type should not be empty string';
+		}
+		if (itemToCheck === 'type') {
+			if (!(/^[a-zA-Z0-9]+$/.test(value))) {
+				throw 'Type contains bad characters';
 			}
 		}
-
-		var domElement = {
-			//Properties
-			get type() {
-				return this._type;
-			},
-			set type(value) {
-				checkTypeName(value, 'type');
-				this._type = value;
-			},
-			get innerHTML() {
-				return '<' + this.type + '></' + this.type + '>';
-			},
-			
-			//Methods
-			init: function (type) {
-				this.type = type;
-				this.attributes = {};
-				return this;
-			},
-			appendChild: function (child) {
-
-			},
-			addAttribute: function (name, value) {
-				checkTypeName(name, 'name');
-				this.attributes[name] = value;
-				return this;
-			},
-			removeAttribute: function (name) {
-				if (this.attributes[name]) {
-					delete this.attributes[name];
-					return this;
-				}
-				throw 'Attribute does not exist';
-
+		else {
+			if (/\d/.test(value)) {
+				throw 'Name contains digits'
 			}
-		};
-		return domElement;
-	} ());
+		}
+	}
 
+	var domElement = {
+		//Properties
+		get type() {
+			return this._type;
+		},
+		set type(value) {
+			checkTypeName(value, 'type');
+			this._type = value;
+		},
+		get attributes() {
+			return this._attributes;
+		},
+		set attributes(value) {
+			this._attributes = value;
+			return this;
+		},
+		get children() {
+			return this._children;
+		},
+		set children(value) {
+			this._children = value;
+		},
+		get parent() {
+			return this._parent;
+		},
+		set parent(value) {
+			this._parent = value;
+		},
+		get content() {
+			return this._content;
+		},
+		set content(value) {
+			this._content = value;
+		},
+		get innerHTML() {
+			var html = '<' + this.type,
+				keys = [],
+				k,
+				i,
+				len,
+				childrenLen,
+				children;
+				
+			for (k in this.attributes) {
+				if (this.attributes.hasOwnProperty(k)) {
+					keys.push(k);
+				}
+			}
+			
+			keys.sort();
+			len = keys.length;
+			
+			for ( i = 0; i < len; i += 1) {
+				html += ' ';
+				html += keys[i];
+				html += '="';
+				html += this.attributes[keys[i]];
+				html += '"';
+			}
+			
+			html += '>';
+			if (this.children.length > 0) {
+				childrenLen = this.children.length;
+				children = this.children;
+				for (i = 0; i < childrenLen; i += 1) {
+					if (typeof children[i] === 'string') {
+						html += children[i];
+					}
+					else {
+						html += children[i].innerHTML;
+					}
+				}
+			}
+			else if(this.content) {
+				html += this.content;
+			}
+			html += '</' + this.type + '>';
+			return html;
+			//return '<' + this.type + '></' + this.type + '>';
+		},
+		
+		//Methods
+		init: function (type) {
+			this.type = type,
+			this.attributes = {},
+			this.parent,
+			this.content,
+			this.children = [];
+			return this;
+		},
+		appendChild: function (child) {
+			this.children.push(child);
+			child.parent = this;
+			return this;
+		},
+		addAttribute: function (name, value) {
+			checkTypeName(name, 'name');
+			this.attributes[name] = value;
+			return this;
+		},
+		removeAttribute: function (name) {
+			if (this.attributes[name]) {
+				delete this.attributes[name];
+				return this;
+			}
+			
+			throw 'Attribute does not exist';
+		}
+	};
+	
+	return domElement;
+} ());
 	return domElement;
 }
 
