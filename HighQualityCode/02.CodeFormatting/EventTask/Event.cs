e3fd2f675615c692fd9664023269b16d06f06733
puhabg <puhabg@live.com>
2015-07-03 16:41:29 +0300

    Adds Event Task in High Quality Code

---
+++ HighQualityCode/02.CodeFormatting/EventTask/Event.cs
@@ -0,0 +1,57 @@
+﻿namespace EventTask
+{
+    using System;
+    using System.Text;
+
+    public class Event : IComparable
+    {
+        public Event(DateTime date, string title, string location)
+        {
+            this.Date = date;
+            this.Title = title;
+            this.Location = location;
+        }
+
+        public DateTime Date { get; private set; }
+
+        public string Title { get; private set; }
+
+        public string Location { get; private set; }
+
+        public int CompareTo(object obj)
+        {
+            Event other = obj as Event;
+            int comparedByDate = this.Date.CompareTo(other.Date);
+            int comparedByTitle = this.Title.CompareTo(other.Title);
+            int comparedByLocation = this.Location.CompareTo(other.Location);
+            if (comparedByDate == 0)
+            {
+                if (comparedByTitle == 0)
+                {
+                    return comparedByLocation;
+                }
+                else
+                {
+                    return comparedByTitle;
+                }
+            }
+            else
+            {
+                return comparedByDate;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder toString = new StringBuilder();
+            toString.Append(this.Date.ToString("yyyy-MM-ddTHH:mm:ss"));
+            toString.Append(" | " + this.Title);
+            if (this.Location != null && this.Location != string.Empty)
+            {
+                toString.Append(" | " + this.Location);
+            }
+
+            return toString.ToString();
+        }
+    }
+}
