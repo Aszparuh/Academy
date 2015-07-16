04fff0c87c24475a13ac9ae724ec3434799caaa3
puhabg <puhabg@live.com>
2015-07-17 00:22:52 +0300

    Refactors Print Statistic Class

---
+++ HighQualityCode/07.VariablesDataExpressionsConstants/ArrayStatisticPrinter/Models/StatisticPrinter.cs
@@ -0,0 +1,35 @@
+﻿namespace ArrayStatisticPrinter.Models
+{
+    using System;
+    using System.Linq;
+
+    public static class StatisticPrinter
+    {
+        public static void PrintStatistic(double[] statistic)
+        {
+            double minimal = GetMin(statistic);
+            double maximal = GetMax(statistic);
+            double average = GetAverage(statistic);
+
+            Console.WriteLine("Minimal = {0}, Maximal = {1}, Average = {2}", minimal, maximal, average);
+        }
+
+        private static double GetMin(double[] array)
+        {
+            var minimal = array.Min();
+            return minimal;
+        }
+
+        private static double GetMax(double[] array)
+        {
+            var maximal = array.Max();
+            return maximal;
+        }
+
+        private static double GetAverage(double[] array)
+        {
+            var average = array.Average();
+            return average;
+        }
+    }
+}
---
+++ HighQualityCode/07.VariablesDataExpressionsConstants/ArrayStatisticPrinter/TestStatisticPrinter.cs
@@ -0,0 +1,13 @@
+﻿namespace ArrayStatisticPrinter
+{
+    using ArrayStatisticPrinter.Models;
+
+    class TestStatisticPrinter
+    {
+        static void Main()
+        {
+            double[] testArray = new double[] { 2.567, 356, 0.43234, 897 };
+            StatisticPrinter.PrintStatistic(testArray);
+        }
+    }
+}
