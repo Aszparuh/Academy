c787e1cd4aa473f007f4ae19f4bc55271e49b130
puhabg <puhabg@live.com>
2015-07-16 14:55:10 +0300

    Solves Bounding Box task

---
+++ HighQualityCode/07.VariablesDataExpressionsConstants/SizeOfRotatedRectangle/SizeCalculations.cs
@@ -0,0 +1,16 @@
+﻿namespace SizeOfRotatedRectangle
+{
+    using System;
+    using SizeOfRotatedRectangle.Models;
+
+    class SizeCalculations
+    {
+        static void Main()
+        {
+            var testRectangle = new Rectangle(25.3, 37.5);
+            Console.WriteLine(testRectangle);
+            var boxOfTestRectangle = testRectangle.GetBoundingBox(90);
+            Console.WriteLine(boxOfTestRectangle);
+        }
+    }
+}
