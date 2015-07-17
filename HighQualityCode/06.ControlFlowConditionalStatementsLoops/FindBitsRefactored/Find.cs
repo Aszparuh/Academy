25ab2624180b2836ce996c06c82e25935fc7fcd8
puhabg <puhabg@live.com>
2015-07-17 16:54:05 +0300

    Adds refactored solutions form exam C#1

---
+++ HighQualityCode/06.ControlFlowConditionalStatementsLoops/FindBitsRefactored/Find.cs
@@ -0,0 +1,38 @@
+﻿namespace FindBitsRefactored
+{
+    using System;
+
+    public class Find
+    {
+        public static int CountStringOccurrences(string text, string pattern)
+        {
+            int count = 0;
+            int i = 0;
+            while ((i = text.IndexOf(pattern, i)) != -1)
+            {
+                i++;
+                count++;
+            }
+
+            return count;
+        }
+
+        public static void Main()
+        {
+            ///input
+            int s = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            string patternToSearch = Convert.ToString(s, 2).PadLeft(5, '0');
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNumber = int.Parse(Console.ReadLine());
+                string numberAsBinaryString = Convert.ToString(currentNumber, 2).PadLeft(29, '0');
+                counter += CountStringOccurrences(numberAsBinaryString, patternToSearch);
+            }
+            ////string numberAsString = Convert.ToString(n, 2).PadLeft(29, '0');
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ HighQualityCode/06.ControlFlowConditionalStatementsLoops/SymbolToNumberRefactored/Solve.cs
@@ -0,0 +1,51 @@
+﻿namespace SymbolToNumberRefactored
+{
+    using System;
+
+    public class Solve
+    {
+        public static void PrintAndCalculate(int currentNumber, int position)
+        {
+            if (position % 2 == 0)
+            {
+                Console.WriteLine("{0:0.00}", (double)currentNumber / 100);
+            }
+            else
+            {
+                Console.WriteLine("{0}", currentNumber * 100);
+            }
+        }
+
+        public static void Main()
+        {
+            ////intput
+            int secret = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            int length = text.Length;
+            ////logic
+            for (int i = 0; i < length; i++)
+            {
+                if (text[i] == '@')
+                {
+                    break;
+                }
+
+                if (char.IsDigit(text[i]))
+                {
+                    int currentNumber = (int)text[i] + secret + 500;
+                    PrintAndCalculate(currentNumber, i);
+                }
+                else if (char.IsLetter(text[i]))
+                {
+                    int currentNumber = ((int)text[i] * secret) + 1000;
+                    PrintAndCalculate(currentNumber, i);
+                }
+                else
+                {
+                    int currentNumber = (int)text[i] - secret;
+                    PrintAndCalculate(currentNumber, i);
+                }
+            }
+        }
+    }
+}
