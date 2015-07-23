fffc02116b5f6dd36944fc874cdc2efb5fa681a3
puhabg <puhabg@live.com>
2015-07-24 01:54:58 +0300

    Adds refactored Printer class

--- HighQualityCode/07. HighQualityMethods/QualityMethods/MethodsTests.cs
+++ HighQualityCode/07. HighQualityMethods/QualityMethods/MethodsTests.cs
@@ -10,7 +10,13 @@
             var someStudent = new Student("Peter", "Ivanov", DateTime.Now);
             Console.WriteLine(someStudent);
 
-            var area = TriangleAreaCalculator.Calculate(45, 27, 13);
+            Printer.PrintNumberAlignedRight(2000);
+            Printer.PrintNumberAlignedRight(15000);
+            Printer.PrintNumberAsPercentage(0.5);
+            Printer.PrintNumberWithPrecision(0.4764345);
+            Printer.PrintDigitAsWord(7);
+
+            var area = TriangleAreaCalculator.Calculate(45, 27, 13); //exception
             Console.WriteLine(area);
         }
     }
---
+++ HighQualityCode/07. HighQualityMethods/QualityMethods/Models/Printer.cs
@@ -0,0 +1,54 @@
+﻿namespace QualityMethods.Models
+{
+    using System;
+
+    public static class Printer
+    {
+        public static void PrintNumberWithPrecision(double number)
+        {
+            Console.WriteLine("{0:F2}", number);
+        }
+
+        public static void PrintNumberAsPercentage(double number)
+        {
+            Console.WriteLine("{0:0.0%}", number);
+        }
+
+        public static void PrintNumberAlignedRight(double number)
+        {
+            Console.WriteLine("{0,8}", number);
+        }
+
+        public static void PrintDigitAsWord(int number)
+        {
+            string word = string.Empty;
+
+            switch(number)
+            {
+                case 0: word = "zero";
+                    break;
+                case 1: word = "one";
+                    break;
+                case 2: word = "two";
+                    break;
+                case 3: word = "three";
+                    break;
+                case 4: word = "four";
+                    break;
+                case 5: word = "five";
+                    break;
+                case 6: word = "six";
+                    break;
+                case 7: word = "seven";
+                    break;
+                case 8: word = "eight";
+                    break;
+                case 9: word = "nine";
+                    break;
+                default: throw new ArgumentException("Number is not a digit");
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
