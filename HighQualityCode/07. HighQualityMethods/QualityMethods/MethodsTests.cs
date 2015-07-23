6fa676d4722c5eec8084d947f4d2a78d4606d709
puhabg <puhabg@live.com>
2015-07-24 01:06:31 +0300

    Adds refactored Student class and Area Calculator

---
+++ HighQualityCode/07. HighQualityMethods/QualityMethods/MethodsTests.cs
@@ -0,0 +1,17 @@
+﻿namespace QualityMethods
+{
+    using System;
+    using QualityMethods.Models;
+
+    class MethodsTests
+    {
+        static void Main()
+        {
+            var someStudent = new Student("Peter", "Ivanov", DateTime.Now);
+            Console.WriteLine(someStudent);
+
+            var area = TriangleAreaCalculator.Calculate(45, 27, 13);
+            Console.WriteLine(area);
+        }
+    }
+}
---
+++ HighQualityCode/07. HighQualityMethods/QualityMethods/Models/TriangleAreaCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace QualityMethods.Models
+{
+    using System;
+
+    public static class TriangleAreaCalculator
+    {
+        public static double Calculate(double sideA, double sideB, double sideC)
+        {
+            if (sideA <= 0 || sideB <= 0 || sideC <= 0)
+            {
+                throw new ArgumentException("One of the sides is 0 or negative");
+            }
+
+            if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA)
+            {
+                throw new ArgumentException("The sides cannot form triangle Triangle Inequality Theorem");
+            }
+
+            double semiperimeter = (sideA + sideB + sideC) / 2;
+            double area = Math.Sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter -sideC));
+            return area;
+        }
+    }
+}
