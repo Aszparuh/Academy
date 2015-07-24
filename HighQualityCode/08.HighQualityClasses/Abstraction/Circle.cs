0eb2086f91613c90feb17e8e3ea8153e7d9cc504
puhabg <puhabg@live.com>
2015-07-24 16:16:22 +0300

    Adds Tasks and Refactors Abstraction

---
+++ HighQualityCode/08.HighQualityClasses/Abstraction/Circle.cs
@@ -0,0 +1,24 @@
+﻿namespace Abstraction
+{
+    using System;
+
+    public class Circle : Figure
+    {
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+
+        internal override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        internal override double CalculateArea()
+        {
+            return Math.PI * this.Radius * this.Radius;
+        }
+    }
+}
