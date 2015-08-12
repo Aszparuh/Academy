ecaba3e558fb28d8ee576904cdbee132d9705232
puhabg <puhabg@live.com>
2015-08-12 17:06:14 +0300

    Adds matrix properties

--- HighQualityCode/13.Refactoring/RotatingWalkInMatrix/Models/Matrix.cs
+++ HighQualityCode/13.Refactoring/RotatingWalkInMatrix/Models/Matrix.cs
@@ -8,28 +8,73 @@
         private readonly int[,] matrix;
         
         private List<Direction> directions;
-        private int size;
+        private int rows;
+        private int columns;
 
-        public Matrix(int size)
+        public Matrix(int rows, int colums, IList<Direction> directions)
         {
-
+            this.Rows = rows;
+            this.Columns = columns;
+            this.Directions = directions;
+            this.matrix = new int[this.Rows, this.Columns];
         }
 
-        public int Size 
+        public int Rows
         {
             get
             {
-                return this.size;
+                return this.rows;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Rows must be positive number");
+                }
+
+                this.rows = value;
+            }
+        }
+
+        public int Columns 
+        {
+            get
+            {
+                return this.columns;
+            }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Columns must be positive number");
+                }
+
+                this.columns = value;
+            }
+        }
+
+        public IList<Direction> Directions 
+        { 
+            get
+            {
+                return this.directions;
             }
 
             set
             {
-                if (value <= 0)
+                if (value == null)
                 {
-                    throw new ArgumentException("Size must be positive number");
+                    throw new ArgumentNullException("Directions cannot be null");
                 }
 
-                this.size = value;
+                if (value.Count == 0)
+                {
+                    throw new ArithmeticException("Directions cannot be empty");
+                }
+
+                this.directions = new List<Direction>(value);
             }
         }
     }
