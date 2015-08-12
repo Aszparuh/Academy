4cf7820e6ec78802154d721ca492f9bc3ada3f1f
puhabg <puhabg@live.com>
2015-08-12 16:19:19 +0300

    Starts refactoring Rotating Walk In Matrix

---
+++ HighQualityCode/13.Refactoring/RotatingWalkInMatrix/Models/Direction.cs
@@ -0,0 +1,21 @@
+﻿namespace RotatingWalkInMatrix.Models
+{
+    class Direction
+    {
+        public Direction(int onRow, int onCol)
+        {
+            this.OnRow = onRow;
+            this.OnCol = onCol;
+        }
+
+        public int OnRow { get; private set; }
+
+        public int OnCol { get; private set; }
+
+        public static Direction operator +(Direction first, Direction second)
+        {
+            var resultDirection = new Direction(first.OnRow + second.OnRow, first.OnCol + second.OnCol);
+            return resultDirection;
+        }
+    }
+}
---
+++ HighQualityCode/13.Refactoring/RotatingWalkInMatrix/Models/Matrix.cs
@@ -0,0 +1,36 @@
+﻿namespace RotatingWalkInMatrix.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Matrix
+    {
+        private readonly int[,] matrix;
+        
+        private List<Direction> directions;
+        private int size;
+
+        public Matrix(int size)
+        {
+
+        }
+
+        public int Size 
+        {
+            get
+            {
+                return this.size;
+            }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Size must be positive number");
+                }
+
+                this.size = value;
+            }
+        }
+    }
+}
