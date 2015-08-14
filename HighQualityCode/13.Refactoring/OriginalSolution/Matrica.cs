dda4b78359ecd7c212680d08e3250256968fafea
puhabg <puhabg@live.com>
2015-08-14 14:20:35 +0300

    Makes the original solution testable

--- HighQualityCode/13.Refactoring/OriginalSolution/Matrica.cs
+++ HighQualityCode/13.Refactoring/OriginalSolution/Matrica.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 
 namespace Task3{
-    class WalkInMatrica {
+    public class WalkInMatrica {
         static void change( ref int dx,ref int dy ) {
             int[] dirX = {1,1,1,0, -1,-1,-1,0 };
             
@@ -42,16 +42,16 @@ namespace Task3{
 
         }
 
-        static void Main( string[] args ){
-            //Console.WriteLine( "Enter a positive number " );
-            //string input = Console.ReadLine(  );
-            //int n = 0;
-            //while ( !int.TryParse( input, out n ) || n < 0 || n > 100 )
-            //{
-            //    Console.WriteLine( "You haven't entered a correct positive number" );
-            //    input = Console.ReadLine(  );
-            //}
-            int n = 3;
+        public static void Main(){
+            Console.WriteLine( "Enter a positive number " );
+            string input = Console.ReadLine(  );
+            int n = 0;
+            while ( !int.TryParse( input, out n ) || n < 0 || n > 100 )
+            {
+                Console.WriteLine( "You haven't entered a correct positive number" );
+                input = Console.ReadLine(  );
+            }
+            //int n = 3;
             int[,] matrica = new int[n, n];
             int step = n, k = 1, i = 0, j = 0, dx = 1, dy = 1;
             while( true ){ //malko e kofti tova uslovie, no break-a raboti 100% : )
@@ -64,10 +64,10 @@ namespace Task3{
                 while ( ( i + dx >= n || i + dx < 0 || j + dy >= n || j + dy < 0 || matrica[i + dx, j + dy] != 0 ) ) {change( ref dx, ref dy );}
                 i += dx; j += dy; k++;
             }
-            for( int p = 0; p < n; p++ ){
-                for( int q = 0; q < n; q++ ) Console.Write( "{0,3}", matrica[p, q] );
-                Console.WriteLine(  );                
-            }
+            //for( int p = 0; p < n; p++ ){
+                //for( int q = 0; q < n; q++ ) Console.Write( "{0,3}", matrica[p, q] );
+                //Console.WriteLine(  );                
+            //}
             find_cell( matrica, out i, out j );
             if( i != 0 && j != 0 ) { // taka go napravih, zashtoto funkciqta ne mi davashe da ne si definiram out parametrite
                 dx = 1; dy = 1;
