793ebe67632918319dcba0d306db865f8a58801c
puhabg <puhabg@live.com>
2015-10-06 15:59:17 +0300

    Starts Databases homework

---
+++ Databases/02.ProcessingXML/ExtractAllArtists/Extract.cs
@@ -0,0 +1,62 @@
+﻿namespace ExtractAllArtists
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    internal class Extract
+    {
+        private static void Main()
+        {
+            var document = new XmlDocument();
+            document.Load("../../../catalog.xml");
+            var root = document.DocumentElement;
+
+            var artists = GetAllArtists(root);
+            foreach (var artist in artists)
+            {
+                Console.WriteLine(artist);
+            }
+
+            var albums = GetNumberOfAlbumsForEachArtist(root);
+            foreach (var pair in albums)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+            
+        }
+
+        private static IEnumerable<string> GetAllArtists(XmlElement rootElement)
+        {
+            var result = new HashSet<string>();
+            var artistsCollection = rootElement.GetElementsByTagName("artist");
+
+            foreach (XmlElement element in artistsCollection)
+            {
+                result.Add(element.InnerText);
+            }
+
+            return result;
+        }
+
+        private static IDictionary<string, int> GetNumberOfAlbumsForEachArtist(XmlElement rootNode)
+        {
+            var result = new Dictionary<string, int>();
+            var albums = rootNode.GetElementsByTagName("album");
+
+            foreach (XmlNode album in albums)
+            {
+                var artist = album["artist"].InnerText;
+                if (!result.ContainsKey(artist))
+                {
+                    result[artist] = 0;
+                }
+
+                result[artist]++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Databases/02.ProcessingXML/ExtractAllArtistsAlbumsXpath/Extract.cs
@@ -0,0 +1,41 @@
+﻿namespace ExtractAllArtistsAlbumsXpath
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Xml;
+
+    class Extract
+    {
+        static void Main()
+        {
+            string xPathQuery = "/catalogue/album";
+            var document = new XmlDocument();
+            document.Load("../../../catalog.xml");
+            var allAlbums = document.SelectNodes(xPathQuery);
+            var allArtists = GetArtistsFromCatalog(allAlbums);
+
+            foreach (var pair in allArtists)
+            {
+                Console.WriteLine("{0} - {1}", pair.Key, pair.Value);
+            }
+        }
+
+        private static Dictionary<string, int> GetArtistsFromCatalog(IEnumerable albumsList)
+        {
+            var resultCollection = new Dictionary<string, int>();
+            foreach (XmlNode album in albumsList)
+            {
+                var artistName = album.SelectSingleNode("artist").InnerText;
+                if (!resultCollection.ContainsKey(artistName))
+                {
+                    resultCollection[artistName] = 0;
+                }
+
+                resultCollection[artistName]++;
+            }
+
+            return resultCollection;
+        }
+    }
+}
