00a26a732b3205203134e590c4be07fcca018642
puhabg <puhabg@live.com>
2015-10-28 14:16:46 +0200

    Adds method wich gets the user input

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/Tasks/Tasks/SequenceOperations.cs
@@ -0,0 +1,49 @@
+﻿namespace Tasks
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SequenceOperations
+    {
+        public static List<int> GetInput()
+        {
+            List<int> input = new List<int>();
+            string inputLine;
+            int inputAsInteger;
+
+            while (true)
+            {
+                inputLine = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(inputLine))
+                {
+                    break;
+                }
+
+                if (int.TryParse(inputLine, out inputAsInteger))
+                {
+                    if (inputAsInteger < 0)
+                    {
+                        throw new ArgumentException("You must enter only possitive numbers");
+                    }
+                    else
+                    {
+                        input.Add(inputAsInteger);
+                    }
+                }
+                else
+                {
+                    throw new ArgumentException("The program works with numbers only");
+                }
+
+            }
+
+            return input;
+        }
+
+        static void Main()
+        {
+            var inputCollection = GetInput();
+        }
+    }
+}
