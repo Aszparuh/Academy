c7337017c059b5e15ad8edd93b064dfcd452b83b
puhabg <puhabg@live.com>
2015-10-29 14:57:53 +0200

    Solves Count Occurrences

---
+++ Data-Structures-and-Algorithms/02. Linear-Data-Structures/Tasks/CountOccurencces/Count.cs
@@ -0,0 +1,35 @@
+﻿namespace CountOccurencces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Count
+    {
+        static void Main()
+        {
+            var inputList = new List<int>() { 3, 4, 4, 2, 3, 3, 4, 3, 2 }; ///2 → 2 times 3 → 4 times 4 → 3 times
+            var countDictionary = new Dictionary<int, int>();
+
+            foreach (var number in inputList)
+            {
+                if (countDictionary.ContainsKey(number))
+                {
+                    countDictionary[number] += 1;
+                }
+                else
+                {
+                    countDictionary.Add(number, 1);
+                }
+            }
+
+            var allNumbers = countDictionary.Keys.ToList();
+            allNumbers.Sort();
+
+            foreach (var number in allNumbers)
+            {
+                Console.WriteLine("{0} → {1} times", number, countDictionary[number]);
+            }
+        }
+    }
+}
