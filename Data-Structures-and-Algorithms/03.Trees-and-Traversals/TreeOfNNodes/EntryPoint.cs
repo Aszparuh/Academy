04b6694aee32da7eb1e9731f6732f26dc2855798
puhabg <puhabg@live.com>
2015-11-04 15:35:27 +0200

    Fixes the creation of the tree

--- Data-Structures-and-Algorithms/03.Trees-and-Traversals/TreeOfNNodes/EntryPoint.cs
+++ Data-Structures-and-Algorithms/03.Trees-and-Traversals/TreeOfNNodes/EntryPoint.cs
@@ -10,8 +10,9 @@
         public static void Main()
         {
             Console.WriteLine("Enter the input: ");
-            var treeList = new Dictionary<int, Node<int>>();
-            
+            var numberOfNodes = int.Parse(Console.ReadLine());
+            var treeCollection = new Dictionary<int, Node<int>>();
+
             while (true)
             {
                 var inputLine = Console.ReadLine();
@@ -21,47 +22,29 @@
                 }
                 else
                 {
-                    var parentAndChild = inputLine.Split(' ');
-                    if (parentAndChild.Length == 1)
+                    string[] childParentValues = inputLine.Split(' ');
+                    int parent = int.Parse(childParentValues[0]);
+                    int child = int.Parse(childParentValues[1]);
+
+                    if (treeCollection.ContainsKey(parent))
                     {
-                        var nodeValue = int.Parse(parentAndChild[0]);
-                        treeList.Add(nodeValue, new Node<int>(nodeValue));
+                        var childNode = new Node<int>(child);
+                        childNode.Parent = treeCollection[parent];
+                        treeCollection[child] = childNode;
                     }
                     else
                     {
-                        var parentNodeValue = int.Parse(parentAndChild[0]);
-                        var childNodeValue = int.Parse(parentAndChild[1]);
-
-                        if (treeList.ContainsKey(parentNodeValue))
-                        {
-                            var childNode = new Node<int>(childNodeValue);
-                            childNode.Parent = treeList[parentNodeValue];
-                            treeList[childNode.Value] = childNode; 
-                            treeList[parentNodeValue].Children.Add(childNode);
-                        }
-                        else
-                        {
-                            var parentNode = new Node<int>(parentNodeValue);
-                            var childNode = new Node<int>(childNodeValue);
-
-                            parentNode.Children.Add(childNode);
-                            childNode.Parent = parentNode;
-
-                            treeList[parentNode.Value] = parentNode;
-                            treeList[childNode.Value] = childNode;
-                        }
+                        var childNode = new Node<int>(child);
+                        var parentNode = new Node<int>(parent);
+                        parentNode.Children.Add(childNode);
+                        childNode.Parent = parentNode;
+                        treeCollection[parent] = parentNode;
+                        treeCollection[child] = childNode;
                     }
                 }
             }
 
-            ///Find the root of the tree;
-            foreach (var element in treeList)
-            {
-                if (element.Value.Parent == null)
-                {
-                    Console.WriteLine("The root is: {0}", element.Value.Value);
-                }
-            }
+            ///Console.WriteLine(treeCollection.Count);
         }
     }
 }
