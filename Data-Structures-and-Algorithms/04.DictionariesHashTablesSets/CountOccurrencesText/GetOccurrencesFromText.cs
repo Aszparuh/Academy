66e801bdfb4b50805bb61000a245361a16a25d8a
puhabg <puhabg@live.com>
2015-11-05 17:44:28 +0200

    Starts solving Count Occurrences in a Text File

---
+++ Data-Structures-and-Algorithms/04.DictionariesHashTablesSets/CountOccurrencesText/GetOccurrencesFromText.cs
@@ -0,0 +1,37 @@
+﻿namespace CountOccurrencesText
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+
+    public class GetOccurrencesFromText
+    {
+        private static string ReadFile(string path)
+        {
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string allText = reader.ReadToEnd();
+                    return allText;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Can't read file");
+            }
+        }
+
+        private static Dictionary<string, int> CountOccurrencesText(string text)
+        {
+
+        }
+
+        public static void Main()
+        {
+            string path = @"../../test.txt";
+            string textFromFile = ReadFile(path);
+            
+        }
+    }
+}
