3e977e456112a8f3556c28007d481032b2cb92f0
puhabg <puhabg@live.com>
2015-11-09 13:28:58 +0200

    Starts Implementaion of Priority Queue

---
+++ Data-Structures-and-Algorithms/07.AdvanceDataStructures/CustomPriorityQueue/PriorityQueue.cs
@@ -0,0 +1,28 @@
+﻿namespace CustomPriorityQueue
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class PriorityQueue<TPriority, TValue>
+    {
+        private List<KeyValuePair<TPriority, TValue>> heapBase;
+        private IComparer<TPriority> comparer;
+
+        public PriorityQueue(IComparer<TPriority> comparer)
+        {
+            this.Comparer = comparer;
+        }
+
+        public IComparer<TPriority> Comparer 
+        { 
+            get; 
+            private set
+            {
+                if (comparer == null)
+                {
+                    throw new ArgumentException("Comparer cannot be null");
+                }
+            }
+        }
+    }
+}
