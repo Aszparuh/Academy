487b906b1e0b1eb098d7e0d26160da75c1601901
puhabg <puhabg@live.com>
2016-01-21 17:12:14 +0200

    Adds Web Forms Homework

---
+++ Web/ASP.NET WebForms/IntroductionHomework/WebFormsSumatorConverter/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebFormsSumatorConverter._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+        <h3>Sumator</h3>
+        <div class="input-group input-group-lg">
+            <asp:TextBox ID="tbFirst" runat="server"></asp:TextBox> 
+        </div>  
+        <div class="input-group input-group-lg">
+            <asp:TextBox ID="tbSecond" runat="server"></asp:TextBox>
+        </div>
+        <div class="input-group">
+            <asp:Button ID="sumButton" runat="server" OnClick="btn_Click"/>
+        </div>
+        <div class="input-group input-group-lg">   
+            <asp:Label ID="result" runat="server"></asp:Label>  
+        </div>           
+    </div>
+</asp:Content>
---
+++ Web/ASP.NET WebForms/IntroductionHomework/WebFormsSumatorConverter/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormsSumatorConverter
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TextBox firstTextBox = this.tbFirst;
+            TextBox secondTextBox = this.tbSecond;
+            Button sumButton = this.sumButton;
+
+            firstTextBox.Attributes["placeholder"] = "Enter number";
+            firstTextBox.Attributes["class"] = "form-control";
+            firstTextBox.Attributes["type"] = "number";
+
+            secondTextBox.Attributes["placeholder"] = "Enter number";
+            secondTextBox.Attributes["class"] = "form-control";
+            secondTextBox.Attributes["type"] = "number";
+
+            sumButton.Attributes["class"] = "btn btn-default";
+            sumButton.Text = "Sum";
+        }
+
+        protected void btn_Click(object sender, EventArgs e)
+        {
+            var result = this.result;
+            var firstNumber = double.Parse(this.tbFirst.Text);
+            var secondNumber = double.Parse(this.tbSecond.Text);
+            var sum = firstNumber + secondNumber;
+
+            result.Text = string.Format("{0} + {1} = {2}", firstNumber, secondNumber, sum);
+        }
+    }
+}
