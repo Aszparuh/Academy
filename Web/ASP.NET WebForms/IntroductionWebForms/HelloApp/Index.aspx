02835364fa37ddc16d9586693728e7fbea545ee7
puhabg <puhabg@live.com>
2016-01-23 16:14:39 +0200

    Adds Introduction to Web Forms Homework

---
+++ Web/ASP.NET WebForms/IntroductionWebForms/HelloApp/Index.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="HelloApp.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <input id="tb" runat="server" placeholder="Enter your name"/>
+            <button id="button" runat="server" onserverclick="OnButtonClick">Greet me</button>
+            <span id="span" runat="server"></span>
+        </div>
+        <div id="div" runat="server"></div>
+    </form>
+</body>
+</html>
---
+++ Web/ASP.NET WebForms/IntroductionWebForms/HelloApp/Index.aspx.cs
@@ -0,0 +1,80 @@
+﻿namespace HelloApp
+{
+    using System;
+    using System.Web.UI;
+
+    public partial class Index : Page
+    {
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            AddParagraph("Page_PreInit invoked");
+        }
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            AddParagraph("Page_Init invoked");
+        }
+
+        protected void Page_InitComplete(object sender, EventArgs e)
+        {
+            AddParagraph("Page_InitComplete invoked");
+        }
+
+        protected void Page_PreLoad(object sender, EventArgs e)
+        {
+            AddParagraph("Page_PreLoad invoked");
+        }
+
+        protected void Page_ControlEvents(object sender, EventArgs e)
+        {
+            AddParagraph("Page_ControlEvents invoked");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            AddParagraph("Page_PreRender invoked");
+        }
+
+        protected void Page_PreRenderComplete(object sender, EventArgs e)
+        {
+            AddParagraph("Page_PreRenderComplete invoked");
+        }
+
+        protected void Page_SaveStateComplete(object sender, EventArgs e)
+        {
+            AddParagraph("Page_SaveStateComplete invoked");
+        }
+
+        protected void Page_RenderComplete(object sender, EventArgs e)
+        {
+            AddParagraph("Page_RenderComplete invoked");
+        }
+
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            AddParagraph("Page_LoadComplete invoked");
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AddParagraph("Page_Load invoked");
+        }
+
+        protected void OnButtonClick(object sender, EventArgs e)
+        {
+            AddParagraph("OnButtonClick invoked");
+            this.span.InnerText = "Hello " + this.tb.Value;
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            AddParagraph("Page_Unload invoked");
+        }
+
+        private void AddParagraph(string text)
+        {
+            string nextParagraph = string.Format("<p>{0}</p>", text);
+            this.div.InnerHtml += nextParagraph;
+        }
+    }
+}
