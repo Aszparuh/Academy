1f263641f975a22cd54b1f978e6adbc035740d73
puhabg <puhabg@live.com>
2016-01-25 22:53:58 +0200

    Adds Calculator

--- Web/ASP.NET WebForms/WebHtmlControls/WebHtmlControls/Calculator.aspx.cs
+++ Web/ASP.NET WebForms/WebHtmlControls/WebHtmlControls/Calculator.aspx.cs
@@ -9,9 +9,82 @@ namespace WebHtmlControls
 {
     public partial class Calculator : System.Web.UI.Page
     {
+        private static long firstNum = 0;
+        private static long secondNum = 0;
+        private static string operation = "";
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
         }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            firstNum = 0;
+            secondNum = 0;
+            operation = "";
+            this.resultTb.Text = "";
+        }
+
+        protected void Number_Click(object sender, CommandEventArgs e)
+        {
+            try
+            {
+                this.resultTb.Text += e.CommandName;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected void Operation_Click(object sender, CommandEventArgs e)
+        {
+            if (e.CommandName == "root")
+            {
+                firstNum = long.Parse(this.resultTb.Text);
+                this.resultTb.Text = Math.Sqrt(firstNum).ToString();
+            }
+            else
+            {
+                firstNum = long.Parse(this.resultTb.Text);
+                operation = e.CommandName;
+                this.resultTb.Text = "";
+            }
+        }
+
+
+        protected void Result_Click(object sender, EventArgs e)
+        {
+            long result = 0;
+            secondNum = long.Parse(this.resultTb.Text);
+            switch (operation)
+            {
+                case "+":
+                    {
+                        result = firstNum + secondNum;
+                    }
+                    break;
+                case "-":
+                    {
+                        result = firstNum - secondNum;
+                    }
+                    break;
+                case "x":
+                    {
+                        result = firstNum * secondNum;
+                    }
+                    break;
+                case "/":
+                    {
+                        result = firstNum / secondNum;
+                    }
+                    break;
+            }
+
+            this.resultTb.Text = result.ToString();
+        }
+
     }
 }
