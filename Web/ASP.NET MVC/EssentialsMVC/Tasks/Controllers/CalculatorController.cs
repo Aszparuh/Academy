9757bd0ca63de979e72a2fb69067fda3e1aa3802
puhabg <puhabg@live.com>
2016-02-08 18:03:43 +0200

    Starts Essentials Homework

---
+++ Web/ASP.NET MVC/EssentialsMVC/Tasks/Controllers/CalculatorController.cs
@@ -0,0 +1,25 @@
+﻿namespace Tasks.Controllers
+{
+    using Models;
+    using System.Web.Mvc;
+
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Values model = new Values();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Values model)
+        {
+            var input = model.Input;
+
+            model.Byte = input / 8;
+            return View(model);
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/EssentialsMVC/Tasks/Models/Values.cs
@@ -0,0 +1,45 @@
+﻿namespace Tasks.Models
+{
+
+    public class Values
+    {
+
+        public double Input { get; set; }
+
+        public double Bit { get; set; }
+
+        public double Byte { get; set; }
+
+        public double Kilobit { get; set; }
+
+        public double Kilobyte { get; set; }
+
+        public double Megabit { get; set; }
+
+        public double Megabyte { get; set; }
+
+        public double Gigabit { get; set; }
+
+        public double Gigabyte { get; set; }
+
+        public double Terabit { get; set; }
+
+        public double Terabyte { get; set; }
+
+        public double Petabit { get; set; }
+
+        public double Petabyte { get; set; }
+
+        public double Exabit { get; set; }
+
+        public double Exabyte { get; set; }
+
+        public double Zettabit { get; set; }
+
+        public double Zetabyte { get; set; }
+
+        public double Yottabit { get; set; }
+
+        public double Yottabyte { get; set; }
+    }
+}
