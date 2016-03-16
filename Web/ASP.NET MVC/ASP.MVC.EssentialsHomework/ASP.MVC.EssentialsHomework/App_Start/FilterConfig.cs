82411150229480ecf2e22ee08f46665b9a14e662
puhabg <puhabg@live.com>
2016-03-16 16:50:15 +0200

    Creates new homework project Creates new area for calculator and both areas shared same layout. Actions in the fixed to work for the area also

---
+++ Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.MVC.EssentialsHomework
+{
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/Areas/Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿namespace ASP.MVC.EssentialsHomework.Areas.Calculator.Controllers
+{
+    using System.Web.Mvc;
+
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator/Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
