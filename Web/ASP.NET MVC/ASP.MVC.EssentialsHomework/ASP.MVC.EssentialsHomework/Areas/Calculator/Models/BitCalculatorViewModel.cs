3024cb6b5646ce853b76d7bbc295b659bf4bf377
puhabg <puhabg@live.com>
2016-03-18 17:26:16 +0200

    Adds ViewModel for Calculator

---
+++ Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/Areas/Calculator/Models/BitCalculatorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ASP.MVC.EssentialsHomework.Areas.Calculator.Models
+{
+    public class BitCalculatorViewModel
+    {
+        public int Quantity { get; set; }
+
+        public Type Type { get; set; }
+
+
+    }
+}
