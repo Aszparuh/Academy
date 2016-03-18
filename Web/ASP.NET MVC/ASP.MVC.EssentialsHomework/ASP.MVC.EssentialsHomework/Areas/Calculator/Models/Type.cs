2eae3008588d48eabd90f10ed76276f08f43dd3c
puhabg <puhabg@live.com>
2016-03-18 17:26:37 +0200

    Adds Type.cs

---
+++ Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/Areas/Calculator/Models/Type.cs
@@ -0,0 +1,24 @@
+﻿namespace ASP.MVC.EssentialsHomework.Areas.Calculator.Models
+{
+    public enum Type
+    {
+       Bit,
+       Byte,
+       Kilobit,
+       Kilobyte,
+       Megabit,
+       Megabyte,
+       Gigabit,
+       Gigabyte,
+       Terabit,
+       Tarabyte,
+       Petabit,
+       Petabyte,
+       Exabit,
+       Exabyte,
+       Zettabit,
+       Zettabyte,
+       Yottabit,
+       Yottabyte
+    }
+}
