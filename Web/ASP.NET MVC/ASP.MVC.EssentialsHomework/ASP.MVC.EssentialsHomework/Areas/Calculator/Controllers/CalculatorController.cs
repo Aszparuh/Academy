54bb173f180e8a4aa7d11b113e832dd6c2a492b8
puhabg <puhabg@live.com>
2016-03-19 17:26:31 +0200

    Adds action for post

--- Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/Areas/Calculator/Controllers/CalculatorController.cs
+++ Web/ASP.NET MVC/ASP.MVC.EssentialsHomework/ASP.MVC.EssentialsHomework/Areas/Calculator/Controllers/CalculatorController.cs
@@ -1,11 +1,19 @@
 ﻿namespace ASP.MVC.EssentialsHomework.Areas.Calculator.Controllers
 {
+    using Models;
     using System.Web.Mvc;
 
     public class CalculatorController : Controller
     {
         // GET: Calculator/Calculator
+        [HttpGet]
         public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(BitCalculatorViewModel model)
         {
             return View();
         }
