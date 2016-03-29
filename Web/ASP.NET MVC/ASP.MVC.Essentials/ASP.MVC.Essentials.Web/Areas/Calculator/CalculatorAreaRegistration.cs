2b9b64f69224aea5564c4e02592441d2c9cd081d
puhabg <puhabg@live.com>
2016-03-29 18:14:10 +0300

    Adds renamed template It is for the MVC Essentials homework

---
+++ Web/ASP.NET MVC/ASP.MVC.Essentials/ASP.MVC.Essentials.Web/Areas/Calculator/CalculatorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿namespace ASP.MVC.Essentials.Web.Areas.Calculator
+{
+    using System.Web.Mvc;
+
+    public class CalculatorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Calculator";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Calculator_default",
+                "Calculator/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/ASP.MVC.Essentials/ASP.MVC.Essentials.Web/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿namespace ASP.MVC.Essentials.Web
+{
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            ViewEnginesConfig.RegisterViewEngines(ViewEngines.Engines);
+
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
