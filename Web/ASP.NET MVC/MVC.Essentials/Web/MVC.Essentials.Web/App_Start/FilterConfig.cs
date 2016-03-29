8bcb234f4e24c4aa5db1616eedf90fc48c14dbd8
puhabg <puhabg@live.com>
2016-03-29 18:17:34 +0300

    Adds renamed template It is for the MVC Essentials homework

---
+++ Web/ASP.NET MVC/MVC.Essentials/Web/MVC.Essentials.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC.Essentials.Web
+{
+    using System.Web.Mvc;
+
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/MVC.Essentials/Web/MVC.Essentials.Web/ViewModels/Account/ExternalLoginListViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MVC.Essentials.Web.ViewModels.Account
+{
+    public class ExternalLoginListViewModel
+    {
+        public string ReturnUrl { get; set; }
+    }
+}
