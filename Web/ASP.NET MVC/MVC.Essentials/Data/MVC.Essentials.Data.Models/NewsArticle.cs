d61c709d9bcfa060707947a6621726d9801a50e6
puhabg <puhabg@live.com>
2016-04-08 16:37:27 +0300

    Adds models for the news app

---
+++ Web/ASP.NET MVC/MVC.Essentials/Data/MVC.Essentials.Data.Models/NewsArticle.cs
@@ -0,0 +1,22 @@
+﻿namespace MVC.Essentials.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Common.Models;
+
+    public class NewsArticle : BaseModel<int>
+    {
+        [Required]
+        [MaxLength(3000)]
+        public string Content { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+
+        public virtual NewsCategory Category { get; set; }
+
+        [Required]
+        public int RegionId { get; set; }
+
+        public virtual Region Region { get; set; }
+    }
+}
