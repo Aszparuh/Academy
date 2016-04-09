f59e611c7151c6391d79d915bb88017adaac99e6
puhabg <puhabg@live.com>
2016-04-09 16:14:52 +0300

    Adds seeded admin with role

--- Web/ASP.NET MVC/MVC.Essentials/Data/MVC.Essentials.Data/Migrations/Configuration.cs
+++ Web/ASP.NET MVC/MVC.Essentials/Data/MVC.Essentials.Data/Migrations/Configuration.cs
@@ -1,6 +1,11 @@
 ﻿namespace MVC.Essentials.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using Models;
 
     public sealed class Configuration : DbMigrationsConfiguration<MVC.Essentials.Data.ApplicationDbContext>
     {
@@ -23,6 +28,37 @@
             ////      new Person { FullName = "Brice Lambson" },
             ////      new Person { FullName = "Rowan Miller" }
             ////    );
+
+            if (!context.Users.Any())
+            {
+                var roleStore = new RoleStore<IdentityRole>(context);
+                var roleManager = new RoleManager<IdentityRole>(roleStore);
+                var userStore = new UserStore<ApplicationUser>(context);
+                var userManager = new UserManager<ApplicationUser>(userStore);
+
+                // Add missing roles
+                var role = roleManager.FindByName("Admin");
+                if (role == null)
+                {
+                    role = new IdentityRole("Admin");
+                    roleManager.Create(role);
+                }
+
+                // Create test users
+                var user = userManager.FindByName("admin");
+                if (user == null)
+                {
+                    var newUser = new ApplicationUser()
+                    {
+                        UserName = "admin",
+                        Email = "admin@admin.bg",
+                        PhoneNumber = "5551234567",
+                    };
+                    userManager.Create(newUser, "Password1");
+                    userManager.SetLockoutEnabled(newUser.Id, false);
+                    userManager.AddToRole(newUser.Id, "Admin");
+                }
+            }
         }
     }
 }
