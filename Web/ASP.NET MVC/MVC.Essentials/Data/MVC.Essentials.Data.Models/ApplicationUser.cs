397517ffed56126cdf560b2d4c4a50de546262c5
puhabg <puhabg@live.com>
2016-04-10 14:30:33 +0300

    ApplicationUser implements IDeletableEntity and IauditInfo

--- Web/ASP.NET MVC/MVC.Essentials/Data/MVC.Essentials.Data.Models/ApplicationUser.cs
+++ Web/ASP.NET MVC/MVC.Essentials/Data/MVC.Essentials.Data.Models/ApplicationUser.cs
@@ -1,15 +1,31 @@
 ﻿namespace MVC.Essentials.Data.Models
 {
+    using System;
     using System.Security.Claims;
     using System.Threading.Tasks;
 
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using Common.Models;
 
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IDeletableEntity, IAuditInfo
     {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        public ApplicationUser()
+        {
+            // TODO: maybe UtcNow, but left it like this for consistency with other code
+            this.CreatedOn = DateTime.Now;
+        }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+    public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
