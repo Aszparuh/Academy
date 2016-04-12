c7ade0e77a82c4c37286cc717b1d693ac437ad2f
puhabg <puhabg@live.com>
2016-04-12 19:31:32 +0300

    Adds try/catch block the errors in the seed method are shown in the console

--- Web/ASP.NET MVC/MvcEssentials/Data/MvcEssentials.Data/Migrations/Configuration.cs
+++ Web/ASP.NET MVC/MvcEssentials/Data/MvcEssentials.Data/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 ﻿namespace MvcEssentials.Data.Migrations
 {
     using System.Data.Entity.Migrations;
+    using System.Data.Entity.Validation;
     using System.Linq;
+    using System.Text;
     using Microsoft.AspNet.Identity;
     using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -81,9 +83,23 @@
                     context.SaveChanges();
                 }
             }
-            catch (System.Exception ex)
+            catch (DbEntityValidationException ex)
             {
-                throw;
+                var sb = new StringBuilder();
+                foreach (var failure in ex.EntityValidationErrors)
+                {
+                    sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+                    foreach (var error in failure.ValidationErrors)
+                    {
+                        sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+                        sb.AppendLine();
+                    }
+                }
+
+                throw new DbEntityValidationException(
+                    "Entity Validation Failed - errors follow:\n" +
+                    sb.ToString(), ex
+                );
             }
         }
     }
