bb107b2dc371b3af5e7d7babc59ef9f95f5f26a8
puhabg <puhabg@live.com>
2016-04-14 15:29:53 +0300

    Adds IndexViewModel

---
+++ Web/ASP.NET MVC/MvcEssentials/Web/MvcEssentials.Web/ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MvcEssentials.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class IndexViewModel
+    {
+        public IEnumerable<NewsArticleViewModel> Articles { get; set; }
+
+        public IEnumerable<NewsCategoryViewModel> Categories { get; set; }
+
+        public IEnumerable<RegionViewModel> Regions { get; set; }
+    }
+}
