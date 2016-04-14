bfc5706c7befd1d4307d97b1ae67c0e6cde8a2dd
puhabg <puhabg@live.com>
2016-04-14 15:24:21 +0300

    Adds Category Service

---
+++ Web/ASP.NET MVC/MvcEssentials/Services/MvcEssentials.Services.Data/NewsCategoryService.cs
@@ -0,0 +1,22 @@
+﻿namespace MvcEssentials.Services.Data
+{
+    using System.Linq;
+
+    using MvcEssentials.Data.Models;
+    using MvcEssentials.Data.Common;
+
+    public class NewsCategoryService : INewsCategoryService
+    {
+        private readonly IDbRepository<NewsCategory> categories;
+
+        public NewsCategoryService(IDbRepository<NewsCategory> categories)
+        {
+            this.categories = categories;
+        }
+
+        public IQueryable<NewsCategory> GetAll()
+        {
+            return this.categories.All().OrderBy(c => c.Name);
+        }
+    }
+}
