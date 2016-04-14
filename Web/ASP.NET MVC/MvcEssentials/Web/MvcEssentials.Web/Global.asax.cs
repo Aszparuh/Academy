4dad0a8c81878a4c14cd4d3608f03ae6371b2916
puhabg <puhabg@live.com>
2016-04-14 14:48:54 +0300

    Registers AutoMapper

--- Web/ASP.NET MVC/MvcEssentials/Web/MvcEssentials.Web/Global.asax.cs
+++ Web/ASP.NET MVC/MvcEssentials/Web/MvcEssentials.Web/Global.asax.cs
@@ -1,12 +1,14 @@
 ﻿namespace MvcEssentials.Web
 {
     using System.Data.Entity;
+    using System.Reflection;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
     using Data;
     using Data.Migrations;
+    using Infrastructure.Mapping;
 
 #pragma warning disable SA1649 // File name must match first type name
     public class MvcApplication : HttpApplication
@@ -16,6 +18,8 @@
         {
             ViewEnginesConfig.RegisterViewEngines(ViewEngines.Engines);
             AutofacConfig.RegisterAutofac();
+            var autoMapperConfig = new AutoMapperConfig();
+            autoMapperConfig.Execute(Assembly.GetExecutingAssembly());
             Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
 
             AreaRegistration.RegisterAllAreas();
