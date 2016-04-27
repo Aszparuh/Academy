a2d6d83a3ab6323584a562dad2aaf336bc2a0dda
puhabg <puhabg@live.com>
2016-04-27 15:20:20 +0300

    Adds logic for resizing Image and project for constants

---
+++ Web/ASP.NET MVC/MvcEssentials/Services/MvcEssentials.Services.Logic/ImageProcessService.cs
@@ -0,0 +1,40 @@
+﻿namespace MvcEssentials.Services.Logic
+{
+    using System.IO;
+    using System.Drawing;
+
+    using ImageProcessor;
+    using ImageProcessor.Imaging;
+    using ImageProcessor.Imaging.Formats;
+    using Common;
+
+    public class ImageProcessService
+    {
+        public byte[] Resize(byte[] originalImage, int width)
+        {
+            using (var originalImageStream = new MemoryStream(originalImage))
+            {
+                using (var resultImage = new MemoryStream())
+                {
+                    using (var imageFactory = new ImageFactory())
+                    {
+                        var createdImage = imageFactory
+                                .Load(originalImageStream);
+
+                        if (createdImage.Image.Width > width)
+                        {
+                            createdImage = createdImage
+                                .Resize(new ResizeLayer(new Size(width, 0), ResizeMode.Max));
+                        }
+
+                        createdImage
+                            .Format(new JpegFormat { Quality = Constants.ImageQuality })
+                            .Save(resultImage);
+                    }
+
+                    return resultImage.GetBuffer();
+                }
+            }
+        }
+    }
+}
