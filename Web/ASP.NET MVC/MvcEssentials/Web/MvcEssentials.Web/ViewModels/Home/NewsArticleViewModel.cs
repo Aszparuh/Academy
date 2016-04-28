82090307cfd782da64f348b375647188b43c47a7
puhabg <puhabg@live.com>
2016-04-28 17:28:53 +0300

    Adds File Controller and changes the article view model. It maps image file. Changes view to include uploaded images

--- Web/ASP.NET MVC/MvcEssentials/Web/MvcEssentials.Web/ViewModels/Home/NewsArticleViewModel.cs
+++ Web/ASP.NET MVC/MvcEssentials/Web/MvcEssentials.Web/ViewModels/Home/NewsArticleViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace MvcEssentials.Web.ViewModels.Home
 {
-    using System;
+    using System.Linq;
     using System.Web.Mvc;
     using AutoMapper;
     using Infrastructure.Mapping;
@@ -17,10 +17,13 @@
 
         public string UserName { get; set; }
 
+        public int? ThumbnailId { get; set; }
+
         public void CreateMappings(IMapperConfiguration configuration)
         {
             configuration.CreateMap<NewsArticle, NewsArticleViewModel>()
-                .ForMember(x => x.UserName, opt => opt.MapFrom(x => x.Author.UserName));
+                .ForMember(x => x.UserName, opt => opt.MapFrom(x => x.Author.UserName))
+                .ForMember(x => x.ThumbnailId, opt => opt.MapFrom(x => x.Images.Where(img => img.Type == ImageType.Thumbnail).FirstOrDefault().Id));
         }
     }
 }
