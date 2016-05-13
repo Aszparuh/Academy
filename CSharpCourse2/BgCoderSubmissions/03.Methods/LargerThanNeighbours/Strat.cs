a5e4934d716b09fb949e9d1c640aaf4f998eb73e
puhabg <puhabg@live.com>
2016-05-13 21:18:31 +0300

    Adds Larger than neighbours in BgCoder submissions

---
+++ CSharpCourse2/BgCoderSubmissions/03.Methods/LargerThanNeighbours/Strat.cs
@@ -0,0 +1,32 @@
+﻿namespace LargerThanNeighbours
+{
+    using System;
+    using System.Linq;
+
+    class Strat
+    {
+        static void Main()
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            var array = input.Split(new char[] { ' ' }).Select(n => int.Parse(n)).ToArray();
+            int result = CountLargerThanNeighbours(array);
+            Console.WriteLine(result);
+        }
+
+        static int CountLargerThanNeighbours(int[] array)
+        {
+            int counter = 0;
+
+            for (int i = 1; i < array.Length - 1; i++)
+            {
+                if (array[i - 1] < array[i] && array[i] > array[i + 1])
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
