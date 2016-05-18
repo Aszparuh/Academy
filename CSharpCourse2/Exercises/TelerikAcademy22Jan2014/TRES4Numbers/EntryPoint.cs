e58680ef85bc508617014d1f022f4c5cc92c0a6a
puhabg <puhabg@live.com>
2016-05-18 14:33:10 +0300

    Adds solution to TRES4Numbers

---
+++ CSharpCourse2/Exercises/TelerikAcademy22Jan2014/TRES4Numbers/EntryPoint.cs
@@ -0,0 +1,43 @@
+﻿namespace TRES4Numbers
+{
+    using System;
+    using System.Numerics;
+
+    class EntryPoint
+    {
+        static string[] alphabet = new string[]
+        {
+            "LON+",
+            "VK-",
+            "*ACAD",
+            "^MIM",
+            "ERIK|",
+            "SEY&",
+            "EMY>>",
+            "/TEL",
+            "<<DON"
+        };
+
+        static string Convert(BigInteger input)
+        { 
+            string result = string.Empty;
+
+            do
+            {
+                int digit = (int)(input % 9);
+                result = alphabet[digit].ToString() + result;
+                input = input / 9;
+            } while (input != 0);
+
+            return result;
+        }
+
+        
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(Convert(BigInteger.Parse(input)));
+        }
+    }
+}
