6ae05d377fdff182a4e7735344a1cd54e5f78e0f
puhabg <puhabg@live.com>
2016-05-19 01:08:03 +0300

    Adds solution to Kaspichan Numbers

---
+++ CSharpCourse2/Exercises/TelerikAcademy4Feb2013M/KaspichanNumbers/EntryPoint.cs
@@ -0,0 +1,46 @@
+﻿namespace KaspichanNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+            ulong number = ulong.Parse(Console.ReadLine());
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            string upperAlphabet = alphabet.ToUpper();
+            List<string> generatedAlphabet = new List<string>();
+
+            for (int i = 0; i < upperAlphabet.Length; i++)
+            {
+                generatedAlphabet.Add(upperAlphabet[i].ToString());
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < upperAlphabet.Length; j++)
+                {
+                    generatedAlphabet.Add(alphabet[i].ToString() + upperAlphabet[j]);
+                }
+            }
+
+            Console.WriteLine(ConvertToKaspichan(number, generatedAlphabet));
+        }
+
+        static string ConvertToKaspichan(ulong number, List<string> alphabet)
+        {
+            string result = string.Empty;
+
+            do
+            {
+                int index = (int)(number % 256);
+                result = alphabet[index] + result;
+                number = number / 256;
+            } while (number > 0);
+
+            return result;
+        }
+    }
+}
