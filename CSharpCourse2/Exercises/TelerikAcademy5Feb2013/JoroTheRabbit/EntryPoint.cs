97adeb2e4c979a2904d53e9fac6bc3d3b59a7599
puhabg <puhabg@live.com>
2016-05-20 13:57:47 +0300

    Adds solution to Joro The Rabbit

---
+++ CSharpCourse2/Exercises/TelerikAcademy5Feb2013/JoroTheRabbit/EntryPoint.cs
@@ -0,0 +1,42 @@
+﻿namespace JoroTheRabbit
+{
+    //95/100 BgCoder. Author's solution gives the same result
+    using System;
+    using System.Linq;
+    
+    class EntryPoint
+    {
+        static void Main()
+        {
+            var terrain = Console.ReadLine().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(n => int.Parse(n)).ToArray();
+            int maxJumpLength = 0;
+
+            for (int i = 0; i < terrain.Length; i++)
+            {
+                for (int currentStep = 1; currentStep < terrain.Length; currentStep++)
+                {
+                    int currentJumpLenght = 1;
+                    int currentPositionOnTerrain = i;
+                    int next = (currentPositionOnTerrain + currentStep) % terrain.Length;
+
+                    while (terrain[currentPositionOnTerrain] < terrain[next])
+                    {
+                        currentJumpLenght++;
+                        currentPositionOnTerrain = next;
+                        next = (currentPositionOnTerrain + currentStep) % terrain.Length;
+
+                    }
+
+                    if (currentJumpLenght > maxJumpLength)
+                    {
+                        maxJumpLength = currentJumpLenght;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine(maxJumpLength);
+        }
+    }
+}
