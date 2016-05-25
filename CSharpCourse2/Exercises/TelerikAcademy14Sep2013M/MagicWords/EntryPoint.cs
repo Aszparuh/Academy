46f7a78e978a38d7423177860e570c2a83e03204
puhabg <puhabg@live.com>
2016-05-25 17:14:38 +0300

    Starts solving Magic Words

---
+++ CSharpCourse2/Exercises/TelerikAcademy14Sep2013M/MagicWords/EntryPoint.cs
@@ -0,0 +1,34 @@
+﻿namespace MagicWords
+{
+    using System;
+    using System.Collections.Generic;
+
+    class EntryPoint
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> words = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                words.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                ReorderWord(words, i);
+            }
+
+            Console.WriteLine(string.Join(",", words));
+        }
+
+        static void ReorderWord(List<string> words, int wordPosition)
+        {
+            string word = words[wordPosition];
+            int destinationPostion = word.Length % words.Count;
+            words.Insert(destinationPostion, word);
+            words.Remove(word);
+        }
+    }
+}
