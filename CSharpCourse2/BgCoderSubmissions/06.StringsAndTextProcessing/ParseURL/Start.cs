1f843eac4515bbab2c05ca26a17abf35ee3f591d
puhabg <puhabg@live.com>
2016-05-29 16:53:53 +0300

    Adds solution to Parse URL

---
+++ CSharpCourse2/BgCoderSubmissions/06.StringsAndTextProcessing/ParseURL/Start.cs
@@ -0,0 +1,22 @@
+﻿namespace ParseURL
+{
+    using System;
+
+    class Start
+    {
+        static void Main()
+        {
+            string inputUrl = Console.ReadLine();
+            int indexOfColumn = inputUrl.IndexOf(':');
+            Console.WriteLine(string.Format("[protocol] = {0}", inputUrl.Substring(0, indexOfColumn)));
+            int startOfServer = indexOfColumn + 3;
+            int endOfServer = inputUrl.IndexOf('/', startOfServer);
+            Console.WriteLine(string.Format("[server] = {0}", inputUrl.Substring(startOfServer, endOfServer - startOfServer)));
+            Console.WriteLine(string.Format("[resource] = {0}", inputUrl.Substring(endOfServer)));
+
+            //[protocol] = https
+            //[server] = github.com
+            //[resource] = /gentoo/gentoo.git
+    }
+    }
+}
