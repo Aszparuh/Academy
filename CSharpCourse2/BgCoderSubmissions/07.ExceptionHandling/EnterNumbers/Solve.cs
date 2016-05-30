88f0e36d1d997745f8cd0b5597a1dffe0dff3682
puhabg <puhabg@live.com>
2016-05-30 13:07:36 +0300

    Adds submissions for Exception Handling

---
+++ CSharpCourse2/BgCoderSubmissions/07.ExceptionHandling/EnterNumbers/Solve.cs
@@ -0,0 +1,39 @@
+﻿namespace EnterNumbers
+{
+    using System;
+
+    class Solve
+    {
+        static void Main()
+        {
+            int[] numbers = new int[12];
+            numbers[0] = 1;
+            numbers[numbers.Length - 1] = 100;
+            for (int i = 1; i < 11; i++)
+            {
+                numbers[i] = ReadNumber(numbers[i - 1], numbers[numbers.Length - 1]);
+            }
+
+            Console.WriteLine(string.Join(" < ", numbers));
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            try
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number < start || number > end)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+
+                return number;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        } 
+    }
+}
---
+++ CSharpCourse2/BgCoderSubmissions/07.ExceptionHandling/SquareRoot/Solve.cs
@@ -0,0 +1,29 @@
+﻿namespace SquareRoot
+{
+    using System;
+
+    class Solve
+    {
+        static void Main()
+        {
+            try
+            {
+                float inputNumber = float.Parse(Console.ReadLine());
+                if (inputNumber < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine(string.Format("{0:0.000}", Math.Sqrt(inputNumber)));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
