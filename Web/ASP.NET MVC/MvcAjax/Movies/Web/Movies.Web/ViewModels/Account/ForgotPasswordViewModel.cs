5dd5c0a2ebe907b5fe9aad740739f1671085f568
puhabg <puhabg@live.com>
2016-06-07 14:20:27 +0300

    Adds project for Ajax Mvc homework

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Account/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Movies.Web.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
