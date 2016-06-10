1b0e92acd454027fecd11251505ec39c321c3573
puhabg <puhabg@live.com>
2016-06-10 12:19:20 +0300

    Adds data models for Movie App Actor and Movie

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Data/Movies.Data.Models/Actor.cs
@@ -0,0 +1,16 @@
+﻿namespace Movies.Data.Models
+{
+    using System.ComponentModel.DataAnnotations;
+    using Movies.Data.Common.Models;
+
+    public class Actor : BaseModel<int>
+    {
+        [Display(Name = "Actor Name")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Actor name should be between 2 and 50 chars.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Actor Age")]
+        [Range(1, 120, ErrorMessage = "Actor age should be between 1 and 120 years")]
+        public int Age { get; set; }
+    }
+}
