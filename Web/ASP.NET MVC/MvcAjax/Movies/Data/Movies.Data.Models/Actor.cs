b431efac13c40eab264789b966efb2fe80f84669
puhabg <puhabg@live.com>
2016-06-13 17:11:12 +0300

    Adds relation many to many between Movies and Actors

--- Web/ASP.NET MVC/MvcAjax/Movies/Data/Movies.Data.Models/Actor.cs
+++ Web/ASP.NET MVC/MvcAjax/Movies/Data/Movies.Data.Models/Actor.cs
@@ -1,10 +1,18 @@
 ﻿namespace Movies.Data.Models
 {
+    using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using Movies.Data.Common.Models;
 
     public class Actor : BaseModel<int>
     {
+        private ICollection<Movie> movies;
+
+        public Actor()
+        {
+            this.movies = new HashSet<Movie>();
+        }
+
         [Display(Name = "Actor Name")]
         [StringLength(50, MinimumLength = 2, ErrorMessage = "Actor name should be between 2 and 50 chars.")]
         public string Name { get; set; }
@@ -12,5 +20,18 @@
         [Display(Name = "Actor Age")]
         [Range(1, 120, ErrorMessage = "Actor age should be between 1 and 120 years")]
         public int Age { get; set; }
+
+        ICollection<Movie> Movies
+        {
+            get
+            {
+                return this.movies;
+            }
+
+            set
+            {
+                this.movies = value;
+            }
+        }
     }
 }
