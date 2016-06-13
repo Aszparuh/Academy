16f68cb7aef4f913dde7226983d1903a094a6d3b
puhabg <puhabg@live.com>
2016-06-13 17:20:53 +0300

    Enables migrations

--- Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/Global.asax.cs
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/Global.asax.cs
@@ -1,8 +1,11 @@
 ﻿namespace Movies.Web
 {
+    using System.Data.Entity;
     using System.Web.Mvc;
     using System.Web.Optimization;
     using System.Web.Routing;
+    using Data;
+    using Data.Migrations;
 
 #pragma warning disable SA1649 // File name must match first type name
     public class MvcApplication : System.Web.HttpApplication
@@ -10,6 +13,7 @@
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<ApplicationDbContext, Configuration>());
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
