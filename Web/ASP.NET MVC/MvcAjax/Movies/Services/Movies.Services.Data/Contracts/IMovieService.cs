0717763d02c4a446f1115473bca672db8ce614e2
puhabg <puhabg@live.com>
2016-06-17 13:54:14 +0300

    Adds project for database services

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Services/Movies.Services.Data/Contracts/IMovieService.cs
@@ -0,0 +1,18 @@
+﻿namespace Movies.Services.Data
+{
+    using System.Linq;
+    using Movies.Data.Models;
+
+    public interface IMovieService
+    {
+        IQueryable<Movie> GetAll();
+
+        Movie GetById(int id);
+
+        void Add(Movie movie);
+
+        void Delete(int id);
+
+        void Update(int id);
+    }
+}
---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Services/Movies.Services.Data/MovieService.cs
@@ -0,0 +1,42 @@
+﻿namespace Movies.Services.Data
+{
+    using System;
+    using System.Linq;
+    using Movies.Data.Common;
+    using Movies.Data.Models;
+
+    public class MovieService : IMovieService
+    {
+        private readonly IDbRepository<Movie> movies;
+
+        public MovieService(IDbRepository<Movie> movies)
+        {
+            this.movies = movies;
+        }
+
+        public void Add(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Movie> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
