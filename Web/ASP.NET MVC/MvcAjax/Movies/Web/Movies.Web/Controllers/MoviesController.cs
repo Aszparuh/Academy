dd1b6e24718a4a75e6bd461f69b19d5acd8e65cb
puhabg <puhabg@live.com>
2016-06-17 16:55:57 +0300

    Adds MovieController, CreateMovieViewModel and partial view Create

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿namespace Movies.Web.Controllers
+{
+    using System.Web.Mvc;
+
+    public class MoviesController : Controller
+    {
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult Create()
+        {
+            return this.PartialView();
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateMovieViewModel.cs
@@ -0,0 +1,45 @@
+﻿namespace Movies.Web.ViewModels.Movies
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class CreateMovieViewModel : IMapTo<Movie>
+    {
+        private ICollection<Actor> actors;
+
+        public CreateMovieViewModel()
+        {
+            this.actors = new HashSet<Actor>();
+        }
+
+        [Required]
+        [Display(Name = "Title")]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = "Movie title should be between 1 and 200 chars.")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        [StringLength(2000, ErrorMessage = "Describtion has to be less than 2000 symbols.")]
+        public string MovieDesciption { get; set; }
+
+        public ICollection<Actor> Actors
+        {
+            get
+            {
+                return this.actors;
+            }
+
+            set
+            {
+                this.actors = value;
+            }
+        }
+
+        [Display(Name = "Female Actors")]
+        public ICollection<Actor> FemaleActors { get; set; }
+
+        [Display(Name = "Male Actors")]
+        public ICollection<Actor> MaleActors { get; set; }
+    }
+}
