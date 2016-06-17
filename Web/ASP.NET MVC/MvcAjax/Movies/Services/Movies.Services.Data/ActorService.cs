ac334d0fb0531534d91ac1774eee41a720dcb609
puhabg <puhabg@live.com>
2016-06-17 15:10:22 +0300

    Adds Actor service Update is not implemented

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Services/Movies.Services.Data/ActorService.cs
@@ -0,0 +1,39 @@
+﻿namespace Movies.Services.Data
+{
+    using System;
+    using System.Linq;
+    using Movies.Data.Common;
+    using Movies.Data.Models;
+    using Movies.Services.Data.Contracts;
+
+    public class ActorService : IActorService
+    {
+        private readonly IDbRepository<Actor> actors;
+
+        public ActorService(IDbRepository<Actor> actors)
+        {
+            this.actors = actors;
+        }
+
+        public void Add(Actor actor)
+        {
+            this.actors.Add(actor);
+            this.actors.Save();
+        }
+
+        public IQueryable<Actor> GetAll()
+        {
+            return this.actors.All();
+        }
+
+        public Actor GetById(int id)
+        {
+            return this.actors.GetById(id);
+        }
+
+        public void Update(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Services/Movies.Services.Data/Contracts/IActorService.cs
@@ -0,0 +1,16 @@
+﻿namespace Movies.Services.Data.Contracts
+{
+    using System.Linq;
+    using Movies.Data.Models;
+
+    public interface IActorService
+    {
+        IQueryable<Actor> GetAll();
+
+        Actor GetById(int id);
+
+        void Add(Actor actor);
+
+        void Update(int id);
+    }
+}
