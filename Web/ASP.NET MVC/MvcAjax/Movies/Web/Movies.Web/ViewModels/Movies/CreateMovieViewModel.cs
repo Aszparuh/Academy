60053c0e3536f5afb9cecb5e81cfcd717bb15b33
puhabg <puhabg@live.com>
2016-06-19 16:16:00 +0300

    Adds dropdown in CreateMovieViewModel

--- Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateMovieViewModel.cs
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateMovieViewModel.cs
@@ -2,18 +2,12 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
+    using System.Web.Mvc;
     using Data.Models;
     using Infrastructure.Mappings;
 
     public class CreateMovieViewModel : IMapTo<Movie>
     {
-        private ICollection<Actor> actors;
-
-        public CreateMovieViewModel()
-        {
-            this.actors = new HashSet<Actor>();
-        }
-
         [Required]
         [Display(Name = "Title")]
         [StringLength(200, MinimumLength = 1, ErrorMessage = "Movie title should be between 1 and 200 chars.")]
@@ -23,23 +17,14 @@
         [StringLength(2000, ErrorMessage = "Describtion has to be less than 2000 symbols.")]
         public string MovieDesciption { get; set; }
 
-        public ICollection<Actor> Actors
-        {
-            get
-            {
-                return this.actors;
-            }
-
-            set
-            {
-                this.actors = value;
-            }
-        }
-
         [Display(Name = "Female Actors")]
-        public ICollection<Actor> FemaleActors { get; set; }
+        public IEnumerable<SelectListItem> FemaleActors { get; set; }
+
+        public int FemaleActorId { get; set; }
 
         [Display(Name = "Male Actors")]
-        public ICollection<Actor> MaleActors { get; set; }
+        public IEnumerable<SelectListItem> MaleActors { get; set; }
+
+        public int MaleActorId { get; set; }
     }
 }
