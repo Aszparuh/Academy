a4e4868d5d15fa44de5489bc37bf8f7bfd77a2c2
puhabg <puhabg@live.com>
2016-06-20 15:42:51 +0300

    Adds missing properties in CreateMovieViewModel

--- Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateMovieViewModel.cs
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateMovieViewModel.cs
@@ -13,6 +13,18 @@
         [StringLength(200, MinimumLength = 1, ErrorMessage = "Movie title should be between 1 and 200 chars.")]
         public string Title { get; set; }
 
+        [Required]
+        [Range(1900, 2016, ErrorMessage = "Movie year should be after 1900 year.")]
+        public int Year { get; set; }
+
+        [Required]
+        [MaxLength(200, ErrorMessage = "Studio name should not be longer that 200 chars")]
+        public string StudioName { get; set; }
+
+        [Required]
+        [MaxLength(500, ErrorMessage = "Studio address should not be longer that 500 chars")]
+        public string StudioAddress { get; set; }
+
         [Display(Name = "Description")]
         [StringLength(2000, ErrorMessage = "Describtion has to be less than 2000 symbols.")]
         public string MovieDesciption { get; set; }
