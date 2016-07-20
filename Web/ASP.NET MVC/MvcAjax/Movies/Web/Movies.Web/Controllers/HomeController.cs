83736c3e88f24baab07ab01b068256289d261e90
puhabg <puhabg@live.com>
2016-07-20 17:57:38 +0300

    Adds Pageable Index page and logic

--- Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/Controllers/HomeController.cs
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/Controllers/HomeController.cs
@@ -1,16 +1,43 @@
 ﻿namespace Movies.Web.Controllers
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Web;
     using System.Web.Mvc;
+    using Infrastructure.Mappings;
+    using Services.Data.Contracts;
+    using ViewModels.Home;
 
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private const int ItemsPerPage = 10;
+
+        private IMovieService movies;
+
+        public HomeController(IMovieService movies)
         {
-            return this.View();
+            this.movies = movies;
+        }
+
+        public ActionResult Index(int id = 1)
+        {
+            var page = id;
+            var allMoviesCount = this.movies.GetAll().Count();
+            var totalPages = (int)Math.Ceiling(allMoviesCount / (decimal)ItemsPerPage);
+            var moviesToSkip = (page - 1) * ItemsPerPage;
+            var movies = this.movies.GetAll()
+                .OrderBy(x => x.Title)
+                .ThenBy(x => x.Id)
+                .Skip(moviesToSkip).Take(ItemsPerPage)
+                .To<MovieViewModel>().ToList();
+
+            var viewModel = new PagableListMovieViewModel
+            {
+                CurrentPage = page,
+                TotalPages = totalPages,
+                Movies = movies
+            };
+
+            return this.View(viewModel);
         }
 
         public ActionResult About()
---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Home/PagableListMovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Movies.Web.ViewModels.Home
+{
+    using System.Collections.Generic;
+
+    public class PagableListMovieViewModel
+    {
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public IEnumerable<MovieViewModel> Movies { get; set; }
+    }
+}
