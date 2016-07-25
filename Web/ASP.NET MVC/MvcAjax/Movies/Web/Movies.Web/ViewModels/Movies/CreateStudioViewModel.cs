3a6f3d54b099de5003d4866d4e38b1c795170a5f
puhabg <puhabg@live.com>
2016-07-25 17:14:55 +0300

    Adds new view model for Studio

---
+++ Web/ASP.NET MVC/MvcAjax/Movies/Web/Movies.Web/ViewModels/Movies/CreateStudioViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Movies.Web.ViewModels.Movies
+{
+    using System.ComponentModel.DataAnnotations;
+    using Data.Models;
+    using Infrastructure.Mappings;
+
+    public class CreateStudioViewModel : IMapTo<Studio>
+    {
+        [Required]
+        [Display(Name = "Studio Name")]
+        [MaxLength(200, ErrorMessage = "Studio name should not be longer that 200 chars")]
+        public string StudioName { get; set; }
+
+        [Required]
+        [Display(Name = "Studio Address")]
+        [MaxLength(500, ErrorMessage = "Studio address should not be longer that 500 chars")]
+        public string StudioAddress { get; set; }
+    }
+}
