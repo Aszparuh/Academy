d08fc1d8f9083565c39be9003c53cd75f3142926
puhabg <puhabg@live.com>
2016-07-27 17:31:13 +0300

    Adds SchoolSystem for test and tests for student class

---
+++ HighQualityCode/UnitTesting/SchoolSystem/School.cs
@@ -0,0 +1,69 @@
+﻿namespace SchoolSystem
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class School
+    {
+        private string name;
+        private IList<Course> courses;
+
+        public School(string name)
+        {
+            this.Name = name;
+            this.courses = new List<Course>();
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Scholl name cannot be null, empty or whitespace");
+                }
+                else
+                {
+                    this.name = value;
+                }
+            }
+        }
+
+        public void AddCourse(Course course)
+        {
+            if (course == null)
+            {
+                throw new ArgumentNullException("Null course cannot be added");
+            }
+            else if (this.courses.Contains(course))
+            {
+                throw new InvalidOperationException("This course is already added");
+            }
+            else
+            {
+                this.courses.Add(course);
+            }
+        }
+
+        public void RemoveCourse(Course course)
+        {
+            if (course == null)
+            {
+                throw new ArgumentNullException("Null course cannot be removes");
+            }
+            else if (!this.courses.Contains(course))
+            {
+                throw new InvalidOperationException("There is no such a course in the school");
+            }
+            else
+            {
+                this.courses.Remove(course);
+            }
+        }
+    }
+}
