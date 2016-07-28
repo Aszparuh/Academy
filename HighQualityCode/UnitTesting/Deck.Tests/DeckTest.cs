e623054a9bf75bcc12335164004ade5c28a1b512
puhabg <puhabg@live.com>
2016-07-28 17:04:29 +0300

    Adds Santase projects in order to test Deck ReportGenerator works as expected with Nunit

--- HighQualityCode/UnitTesting/Deck.Tests/DeckTest.cs
+++ HighQualityCode/UnitTesting/Deck.Tests/DeckTest.cs
@@ -1,12 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Deck.Tests
+﻿namespace Deck.Tests
 {
-    class DeckTest
+    using NUnit.Framework;
+    using Santase.Logic;
+    using Santase.Logic.Cards;
+    using System;
+
+    [TestFixture]
+    public class DeckTest
     {
+        private const int DeckSize = 24;
+
+        [Test]
+        public void DrawNextCardOnEmptyDeck_ShouldThrowException()
+        {
+            var deck = new Deck();
+
+            for (int card = 0; card < DeckSize; card++)
+            {
+                deck.GetNextCard();
+            }
+
+            Assert.Throws<InternalGameException>(() => deck.GetNextCard());
+        }
     }
 }
