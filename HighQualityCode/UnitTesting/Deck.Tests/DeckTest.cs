23ab307b2c035d3333610b0f98bf5a8661ed3165
puhabg <puhabg@live.com>
2016-07-29 18:05:07 +0300

    Finishes test for deck

--- HighQualityCode/UnitTesting/Deck.Tests/DeckTest.cs
+++ HighQualityCode/UnitTesting/Deck.Tests/DeckTest.cs
@@ -1,9 +1,11 @@
 ﻿namespace Deck.Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
+    using Extensions;
     using NUnit.Framework;
     using Santase.Logic;
     using Santase.Logic.Cards;
-    using System;
 
     [TestFixture]
     public class DeckTest
@@ -30,5 +32,62 @@
 
             Assert.IsNotNull(deck.GetTrumpCard);
         }
+
+        [Test]
+        public void ExpectTheTrumpCardToBeTakenLast()
+        {
+            var deck = new Deck();
+            var trumpCard = deck.GetTrumpCard;
+            Card lastCard = deck.GetNextCard();
+
+            while (deck.CardsLeft > 0)
+            {
+                lastCard = deck.GetNextCard();
+            }
+
+            Assert.AreSame(trumpCard, lastCard);
+        }
+
+        [Test]
+        public void ExpectTheTrumpCardToBeChanged()
+        {
+            var deck = new Deck();
+            var initialTrumpCard = deck.GetTrumpCard;
+            var newTrumpCard = deck.GetNextCard();
+
+            deck.ChangeTrumpCard(newTrumpCard);
+
+            Assert.AreSame(newTrumpCard, deck.GetTrumpCard);
+        }
+
+        [Test]
+        public void TwoEqualCardsAreConsideredTheSame()
+        {
+            var first = new Card(CardSuit.Club, CardType.Ace);
+            var second = new Card(CardSuit.Club, CardType.Ace);
+
+            Assert.AreEqual(first, second);
+        }
+
+        [TestCase(4)]
+        [TestCase(8)]
+        [TestCase(15)]
+        [TestCase(17)]
+        [TestCase(24)]
+        public void ExpectedDeckToReturnNDifferentCards(int count)
+        {
+            var deck = new Deck();
+            var cards = new List<Card>();
+
+            for (int card = 0; card < count; card++)
+            {
+                cards.Add(deck.GetNextCard());
+            }
+
+            var distinc = cards.DistinctBy(c => new { c.Suit, c.Type } );
+
+            Assert.AreEqual(count, distinc.Count());
+        }
+
     }
 }
