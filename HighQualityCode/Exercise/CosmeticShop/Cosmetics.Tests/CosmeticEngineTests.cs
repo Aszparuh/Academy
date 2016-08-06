9e6f0b5f1c8b865e8a024a7a6697f3dce06041e0
puhabg <puhabg@live.com>
2016-08-06 18:44:52 +0300

    Adds exercise for unit testing

---
+++ HighQualityCode/Exercise/CosmeticShop/Cosmetics.Tests/CosmeticEngineTests.cs
@@ -0,0 +1,69 @@
+﻿namespace Cosmetics.Tests
+{
+    using System.Collections.Generic;
+    using Contracts;
+    using Mocked;
+    using Moq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CosmeticEngineTests
+    {
+        [Test]
+        public void Start_WhenInputStringIsValidCreateCategoryCommand_CategoryShouldBeAddedToList()
+        {
+            // Arrange
+            string categoryName = "For Female";
+
+            var mockedFactory = new Mock<ICosmeticsFactory>();
+            var mockedShoppingCart = new Mock<IShoppingCart>();
+            var mockedCommandParser = new Mock<ICommandParser>();
+
+            var mockedCommand = new Mock<ICommand>();
+            mockedCommand.SetupGet(x => x.Name).Returns("CreateCategory");
+            mockedCommand.SetupGet(x => x.Parameters).Returns(new List<string>() { categoryName });
+            mockedCommandParser.Setup(x => x.ReadCommands()).Returns(new List<ICommand>() { mockedCommand.Object });
+
+            var mockedCategory = new Mock<ICategory>();
+            mockedCategory.SetupGet(x => x.Name).Returns(categoryName);
+            mockedFactory.Setup(x => x.CreateCategory(It.IsAny<string>())).Returns(mockedCategory.Object);
+
+            var engine = new MockedEngine(mockedFactory.Object, mockedShoppingCart.Object, mockedCommandParser.Object);
+
+            // Act
+            engine.Start();
+
+            // Assert
+            Assert.IsTrue(engine.Categories.ContainsKey(categoryName));
+            Assert.AreSame(mockedCategory.Object, engine.Categories[categoryName]);
+        }
+
+        public void Start_WhenInputStringIsValidAddProductCommand_ProductShouldBeAddedToList()
+        {
+            // Arrange
+            string categoryName = "For Female";
+
+            var mockedFactory = new Mock<ICosmeticsFactory>();
+            var mockedShoppingCart = new Mock<IShoppingCart>();
+            var mockedCommandParser = new Mock<ICommandParser>();
+
+            var mockedCommand = new Mock<ICommand>();
+            mockedCommand.SetupGet(x => x.Name).Returns("CreateCategory");
+            mockedCommand.SetupGet(x => x.Parameters).Returns(new List<string>() { categoryName });
+            mockedCommandParser.Setup(x => x.ReadCommands()).Returns(new List<ICommand>() { mockedCommand.Object });
+
+            var mockedCategory = new Mock<ICategory>();
+            mockedCategory.SetupGet(x => x.Name).Returns(categoryName);
+            mockedFactory.Setup(x => x.CreateCategory(It.IsAny<string>())).Returns(mockedCategory.Object);
+
+            var engine = new MockedEngine(mockedFactory.Object, mockedShoppingCart.Object, mockedCommandParser.Object);
+
+            // Act
+            engine.Start();
+
+            // Assert
+            Assert.IsTrue(engine.Categories.ContainsKey(categoryName));
+            Assert.AreSame(mockedCategory.Object, engine.Categories[categoryName]);
+        }
+    }
+}
---
+++ HighQualityCode/Exercise/CosmeticShop/Cosmetics.Tests/ValidatorTests.cs
@@ -0,0 +1,59 @@
+﻿namespace Cosmetics.Tests
+{
+    using System;
+    using Common;
+    using NUnit.Framework;
+    using Ploeh.AutoFixture;
+    using Products;
+
+    [TestFixture]
+    public class ValidatorTests
+    {
+        [Test]
+        public void CheckIfNullShould_Throw_NullReferenceException_WhenTheParameterObjIsNull()
+        {
+           object obj = null;
+
+           Assert.Throws<NullReferenceException>(() => Validator.CheckIfNull(obj, string.Empty));
+        }
+
+        [Test]
+        public void CheckIfNullShould_Throw_NullReferenceException_WhenTheParameterObjIsNotNull()
+        {
+            var fixture = new Fixture();
+            var str = fixture.Create<string>();
+
+            Assert.DoesNotThrow(() => Validator.CheckIfNull(str, string.Empty));
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void CheckIfStringIsNullOrEmpty_Should_ThrowNullReferenceException_WhenTheParameterTextIsNullOrEmpty(string text)
+        {
+            Assert.Throws<NullReferenceException>(() => Validator.CheckIfStringIsNullOrEmpty(text));
+        }
+
+        [Test]
+        public void CheckIfStringIsNullOrEmpty_Should_ThrowNullReferenceException_WhenTheParameterTextIsNotNullOrEmpty()
+        {
+            var fixture = new Fixture();
+            var str = fixture.Create<string>();
+
+            Assert.DoesNotThrow(() => Validator.CheckIfStringIsNullOrEmpty(str));
+        }
+
+        [TestCase("two--")]
+        [TestCase("a")]
+        public void CheckIfStringLengthIsValid_Should_ThrowIndexOutOfRangeException_WhenTheLengthOfTheParameterTextIsLowerOrGreaterThanAllowed(string text)
+        {
+            Assert.Throws<IndexOutOfRangeException>(() => Validator.CheckIfStringLengthIsValid(text, 3, 2));
+        }
+
+        [TestCase("two")]
+        [TestCase("aa")]
+        public void CheckIfStringLengthIsValid_ShouldNot_ThrowAnyException_WhenTheLengthOfTheParameterTextIsLowerOrGreaterThanAllowed(string text)
+        {
+            Assert.DoesNotThrow(() => Validator.CheckIfStringLengthIsValid(text, 3, 2));
+        }
+    }
+}
