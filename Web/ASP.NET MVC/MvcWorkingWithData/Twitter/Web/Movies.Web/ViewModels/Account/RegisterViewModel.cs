5bec2c79f2778b48c81261eb32979300494eecfd
puhabg <puhabg@live.com>
2016-08-26 15:03:50 +0300

    Login and register now use Username instead of Email

--- Web/ASP.NET MVC/MvcWorkingWithData/Twitter/Web/Movies.Web/ViewModels/Account/RegisterViewModel.cs
+++ Web/ASP.NET MVC/MvcWorkingWithData/Twitter/Web/Movies.Web/ViewModels/Account/RegisterViewModel.cs
@@ -5,6 +5,11 @@
     public class RegisterViewModel
     {
         [Required]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
