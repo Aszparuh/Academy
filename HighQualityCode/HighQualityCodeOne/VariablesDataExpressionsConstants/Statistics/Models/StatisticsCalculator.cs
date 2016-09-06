f74d65064310eb9a71ffe31d3c0a5b5bad320e13
puhabg <puhabg@live.com>
2016-09-06 13:03:55 +0300

    Adds tasks from Variables, Data, Expressions and Constants Homework

---
+++ HighQualityCode/HighQualityCodeOne/VariablesDataExpressionsConstants/Statistics/Models/StatisticsCalculator.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace Statistics.Models
+{
+    public class StatisticsCalculator
+    {
+        public string GetStatistic(double[] statistic)
+        {
+            double minimal = this.GetMin(statistic);
+            double maximal = this.GetMax(statistic);
+            double average = this.GetAverage(statistic);
+            string statistics = string.Format("Minimal = {0}, Maximal = {1}, Average = {2}", minimal, maximal, average);
+
+            return statistics;
+        }
+
+        private double GetMin(double[] array)
+        {
+            var minimal = array.Min();
+            return minimal;
+        }
+
+        private double GetMax(double[] array)
+        {
+            var maximal = array.Max();
+            return maximal;
+        }
+
+        private double GetAverage(double[] array)
+        {
+            var average = array.Average();
+            return average;
+        }
+    }
+}
---
+++ HighQualityCode/HighQualityCodeOne/VariablesDataExpressionsConstants/TestApplication/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Shapes;
+using Statistics.Models;
+
+namespace TestApplication
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var testRectangle = new Rectangle(25.3, 37.5);
+            Console.WriteLine(testRectangle);
+            var boxOfTestRectangle = testRectangle.GetBoundingBox(90);
+            Console.WriteLine(boxOfTestRectangle);
+            Console.WriteLine();
+
+            double[] numbers = new double[] { 2.567, 356, 0.43234, 897 };
+            var printer = new StatisticsCalculator();
+            var statistic = printer.GetStatistic(numbers);
+            Console.WriteLine(statistic);
+        }
+    }
+}
