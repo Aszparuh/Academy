17108df8bdb66162688b005e9fc60fbd39a36382
puhabg <puhabg@live.com>
2016-10-15 14:25:31 +0300

    Starts Processing XML homework

---
+++ Databases/2016/ProcessingXML/ExtractAllArtists/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml;
+
+namespace ExtractAllArtists
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var path = "../../../DocumentsXML/catalogue.xml";
+            XmlDocument document = new XmlDocument();
+            document.Load(path);
+
+            Console.WriteLine(document.OuterXml);
+        }
+    }
+}
