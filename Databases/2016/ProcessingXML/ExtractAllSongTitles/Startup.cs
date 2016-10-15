d733a2b0d26c4fe61cd71cd034ee1ad8d7e45a33
puhabg <puhabg@live.com>
2016-10-15 17:00:43 +0300

    Finishes project that extracts all the song titles

--- Databases/2016/ProcessingXML/ExtractAllSongTitles/Startup.cs
+++ Databases/2016/ProcessingXML/ExtractAllSongTitles/Startup.cs
@@ -1,9 +1,23 @@
-﻿namespace ExtractAllSongTitles
+﻿using System;
+using System.Xml;
+
+namespace ExtractAllSongTitles
 {
     public class Startup
     {
         public static void Main()
         {
+            var path = "../../../DocumentsXML/catalogue.xml";
+            using (XmlReader reader = XmlReader.Create(path))
+            {
+                while (reader.Read())
+                {
+                    if (reader.Name == "title")
+                    {
+                        Console.WriteLine(reader.ReadElementString());
+                    }
+                }
+            }
         }
     }
 }
