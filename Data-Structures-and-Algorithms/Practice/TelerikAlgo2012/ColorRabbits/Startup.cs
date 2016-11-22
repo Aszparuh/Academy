fc9a6e8edad1e335ec94f2be76746551c09f9fc3
puhabg <puhabg@live.com>
2016-11-22 16:07:33 +0200

    Adds solution to color rabbits

---
+++ Data-Structures-and-Algorithms/Practice/TelerikAlgo2012/ColorRabbits/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ColorRabbits
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var dictionary = new Dictionary<int, int>();
+            int numberOfAskedRabbits = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfAskedRabbits; i++)
+            {
+                var input = int.Parse(Console.ReadLine()) + 1;
+
+                if (dictionary.ContainsKey(input))
+                {
+                    dictionary[input] += 1;
+                }
+                else
+                {
+                    dictionary.Add(input, 1);
+                }
+            }
+
+            int sum = 0;
+            foreach (KeyValuePair<int, int> entry in dictionary)
+            {
+                if (entry.Value == 1)
+                {
+                    sum += entry.Key;
+                }
+                else
+                {
+                    var number = Math.Ceiling((double)entry.Value / (double)entry.Key);
+                    sum += (int)number * entry.Key;
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
