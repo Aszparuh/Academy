9b5477a506d6a8f550f95c18c7173be542bd00a5
puhabg <puhabg@live.com>
2016-11-22 02:12:03 +0200

    Adds project for practicing algorithms

---
+++ Data-Structures-and-Algorithms/Practice/TelerikAlgo2012/BinaryPasswords/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryPasswords
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            int countOfStars = input.Count(s => s == '*');
+
+            long result = 1;
+
+            for (int i = 0; i < countOfStars; i++)
+            {
+                result *= 2;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
