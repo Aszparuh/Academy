e5ec300641960c1ce322ac7c217b5d90abe36b2b
puhabg <puhabg@live.com>
2016-11-29 17:56:22 +0200

    Adds task from workshop

---
+++ Data-Structures-and-Algorithms/Workshop/Passwords/Startup.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Passwords
+{
+    public class Startup
+    {
+        private static List<int[]> sequences;
+        private static int passwordLength;
+        private static string pattern;
+        private static int passwordNumber;
+
+        public static void Main()
+        {
+            passwordLength = int.Parse(Console.ReadLine());
+            pattern = Console.ReadLine();
+            passwordNumber = int.Parse(Console.ReadLine());
+            Solve();
+
+            foreach (var sequence in sequences)
+            {
+                Console.WriteLine(string.Join(",", sequence));
+            }
+        }
+
+        private static void Solve()
+        {
+            sequences = new List<int[]>();
+            if (pattern[0] == '<')
+            {
+                PutSmaller(new int[passwordLength], 0, 9);
+            }
+            else
+            {
+                PutBigger(new int[passwordLength], 0, 0);
+            }
+        }
+
+        private static void PutBigger(int[] sequence, int index, int current)
+        {
+            if (index == passwordLength)
+            {
+                sequences.Add(sequence);
+                return;
+            }
+
+            if (pattern[index] == '<')
+            {
+                PutSmaller(sequence, index, current);
+            }
+
+            for (int i = current + 1; i < 10; i++)
+            {
+                sequence[index] = i;
+                PutBigger(sequence, index + 1, i);  
+            }
+        }
+
+        private static void PutSmaller(int[] sequence, int index, int current)
+        {
+            if (index == passwordLength)
+            {
+                sequences.Add(sequence);
+                return;
+            }
+
+            if (pattern[index] == '>')
+            {
+                PutBigger(sequence, index, current);
+            }
+
+            for (int i = current - 1; i < 0; i--)
+            {
+                sequence[index] = i;
+                PutSmaller(sequence, index + 1, i);
+            }
+        }
+    }
+}
---
+++ Data-Structures-and-Algorithms/Workshop/ReversePolishNotation/Startup.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReversePolishNotation
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var notation = Console.ReadLine().Split(' ');
+            var result = new Stack<int>();
+
+            for (int i = 0; i < notation.Length; i++)
+            {
+                var currentElement = notation[i];
+                int number;
+                if (int.TryParse(currentElement, out number))
+                {
+                    result.Push(number);
+                }
+                else
+                {
+                    var a = result.Pop();
+                    var b = result.Pop();
+
+                    if (notation[i] == "+")
+                    {
+                        result.Push(a + b);
+                    }
+                    else if (notation[i] == "-")
+                    {
+                        result.Push(b - a);
+                    }
+                    else if (notation[i] == "*")
+                    {
+                        result.Push(a * b);
+                    }
+                    else if (notation[i] == "/")
+                    {
+                        result.Push(b / a);
+                    }
+                    else if (notation[i] == "|")
+                    {
+                        result.Push(b | a);
+                    }
+                    else if (notation[i] == "&")
+                    {
+                        result.Push(a & b);
+                    }
+                    else if (notation[i] == "^")
+                    {
+                        result.Push(a ^ b);
+                    }
+                }
+            }
+
+            if (result.Count > 1)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                Console.WriteLine(result.Pop());
+            }
+        }
+    }
+}
