075c99250b77875727818a38e7f317abcaf24d82
puhabg <puhabg@live.com>
2016-12-03 17:43:39 +0200

    Starts solving Quadronacci

---
+++ Data-Structures-and-Algorithms/Practice/DynamicProgramming/TelerikAlgoFeb2014/Quadronacci/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quadronacci
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var list = new List<int>();
+
+            for (int i = 0; i < 4; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int lastNumber = r * c;
+
+            for (int i = 4; i < lastNumber; i++)
+            {
+                list.Add(list[i - 1] + list[i - 2] + list[i - 3] + list[i - 4]);
+            }
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
