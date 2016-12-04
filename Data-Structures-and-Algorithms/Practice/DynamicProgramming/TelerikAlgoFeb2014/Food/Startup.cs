c0ae9b9d86717827c76325f8e551695afe6bc9c6
puhabg <puhabg@live.com>
2016-12-04 16:58:25 +0200

    Starts solution for Food

---
+++ Data-Structures-and-Algorithms/Practice/DynamicProgramming/TelerikAlgoFeb2014/Food/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Food
+{
+    public class Item
+    {
+        public string Name { get; set; }
+
+        public int Weight { get; set; }
+
+        public int Value { get; set; }
+    }
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            int maxFood = int.Parse(Console.ReadLine());
+            int numberOfFoods = int.Parse(Console.ReadLine());
+            var items = new Item[numberOfFoods];
+
+            for (int i = 0; i < numberOfFoods; i++)
+            {
+                var currentSplitedText = Console.ReadLine().Split(' ');
+                var itemName = currentSplitedText[0];
+                var itemWeight = int.Parse(currentSplitedText[1]);
+                var itemValue = int.Parse(currentSplitedText[2]);
+                var item = new Item() { Name = itemName, Weight = itemWeight, Value = itemValue };
+                items[i] = item;
+            }
+
+            int[,] knapsackField = new int[maxFood + 1, numberOfFoods + 1];
+            int[,] backtrack = new int[maxFood + 1, numberOfFoods + 1];
+
+
+        }
+    }
+}
