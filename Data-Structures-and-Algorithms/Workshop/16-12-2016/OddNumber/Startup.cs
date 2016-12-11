b12632b0421aa59c75bf117571bf652fe9e52724
puhabg <puhabg@live.com>
2016-12-11 18:24:07 +0200

    Adds solution to Odd Number

---
+++ Data-Structures-and-Algorithms/Workshop/16-12-2016/OddNumber/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OddNumber
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            int numberOfValues = int.Parse(Console.ReadLine());
+            long num = 0;
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                num ^= long.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
