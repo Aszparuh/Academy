d4b77e2ea6d010769f48fc335e75ac1b0320ede5
puhabg <puhabg@live.com>
2016-12-16 00:26:42 +0200

    Starts another solution to Diameter

--- Data-Structures-and-Algorithms/Workshop/16-12-2016/DiameterV2/Startup.cs
+++ Data-Structures-and-Algorithms/Workshop/16-12-2016/DiameterV2/Startup.cs
@@ -1,58 +1,59 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DiameterV2
 {
     public class Startup
-    {
-        public static int[,] graph;
-        public static int max;
-        public static int maxNode;
-        public static int count;
-        public static bool[] visited;
-
+    {   
         public static void Main()
         {
-            int numberOfNodes = int.Parse(Console.ReadLine());
-            graph = new int[numberOfNodes, numberOfNodes];
-            visited = new bool[numberOfNodes];
+            var graph = new Dictionary<int, Node>();
+            var numberOfNodes = int.Parse(Console.ReadLine());
 
             for (int i = 0; i < numberOfNodes - 1; i++)
             {
                 var input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).ToArray();
-                var row = input[0];
-                var column = input[1];
-                var value = input[2];
+                var first = input[0];
+                var second = input[0];
+                var value = input[0];
 
-                graph[row, column] = value;
-                graph[column, row] = value;
-            }
+                Node firstNode;
+                Node secondNode;
 
-            Dfs(0);
-            visited = new bool[numberOfNodes];
-            Dfs(maxNode);
-            Console.WriteLine(max);
-        }
-
-        private static void Dfs(int node)
-        {
-            visited[node] = true;
-
-            for (int i = 0; i < graph.GetLength(0); i++)
-            {
-                if (visited[i] != true && graph[node, i] != 0)
+                if (graph.ContainsKey(first))
                 {
-                    count += graph[node, i];
-                    Dfs(i);
-                    count -= graph[node, i];
+                    firstNode = graph[first];
                 }
-            }
+                else
+                {
+                    firstNode = new Node();
+                    graph.Add(first, firstNode);
+                }
 
-            if (count > max)
-            {
-                max = count;
-                maxNode = node;
+                if (graph.ContainsKey(second))
+                {
+                    secondNode = graph[second];
+                }
+                else
+                {
+                    secondNode = new Node();
+                    graph.Add(second, secondNode);
+                }
+
+                firstNode.Neighbours.Add(secondNode, value);
+                secondNode.Neighbours.Add(firstNode, value);
             }
         }
+
+        private static void Dfs()
+        {
+           
+        }
+    }
+
+    public class Node
+    {
+        public Dictionary<Node, int> Neighbours { get; set; }
     }
 }
